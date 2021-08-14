b2ba1c038ab13321e1b3935fbb3eca5dbe500c0d
AlexiWolf <alexiwolf@pm.me>
2021-08-14 10:17:39 -0500

    Should log the message to the connected log targets

--- src/logging.rs
+++ src/logging.rs
@@ -6,17 +6,45 @@ pub(crate) fn initialize_logging(level: LevelFilter) -> Result<Logger, ()> {
 
 pub struct Logger;
 
+impl Logger {
+    pub fn add_log_target(&self, log_target: &dyn LogTarget) {}
+}
+
+pub trait LogTarget: 'static + Send + Sync {}
+
 #[cfg(test)]
 mod log_tests {
-    use super::*;
     use super::log_test_fixtures::*;
+    use super::*;
+    use log::info;
 
     #[test]
     fn should_log_to_connected_log_targets() {
-        let logger = initialize_logging(LevelFilter::Trace)
-            .expect("Failed to initialize the logger");
-        let test_log_target = TestLogTarget::new();
-        logger.add_log_target(test_log_target);
+        let logger =
+            initialize_logging(LevelFilter::Trace).expect("Failed to initialize the logger");
+        let log_target_a = TestLogTarget::new();
+        let log_target_b = TestLogTarget::new();
+        logger.add_log_target(&log_target_a);
+        logger.add_log_target(&log_target_b);
+
+        info!("Hello, World!");
+
+        assert_eq!(
+            log_target_a
+                .latest_record()
+                .expect("No message was sent")
+                .args()
+                .to_string(),
+            "Hello, World!".to_string()
+        );
+        assert_eq!(
+            log_target_b
+                .latest_record()
+                .expect("No message was sent")
+                .args()
+                .to_string(),
+            "Hello, World!".to_string()
+        );
     }
 }
 
@@ -31,5 +59,11 @@ pub mod log_test_fixtures {
         pub fn new() -> Self {
             Self
         }
+
+        pub fn latest_record(&self) -> Option<Record> {
+            None
+        }
     }
+
+    impl LogTarget for TestLogTarget {}
 }
