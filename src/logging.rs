275a80600989b88c5c11fdb70ce26d613d2b3d95
AlexiWolf <alexiwolf@pm.me>
2021-08-14 17:34:02 -0500

    Cleaned up code warnings

--- src/logging.rs
+++ src/logging.rs
@@ -1,20 +1,21 @@
-use log::{LevelFilter, Log, Metadata, Record};
 use lazy_static::lazy_static;
+use log::{LevelFilter, Log, Metadata, Record};
 use std::sync::{Arc, Mutex};
 
 lazy_static! {
     pub(crate) static ref LOGGER: Logger = Logger::new();
 }
 
-pub(crate) fn initialize_logging(level: LevelFilter) -> Result<&'static Logger, ()> {
-    log::set_logger(&*LOGGER as &dyn Log)
+pub(crate) fn initialize_logging(level: LevelFilter) -> &'static Logger {
+    let logger = &*LOGGER;
+    log::set_logger(logger as &dyn Log)
         .map(|()| log::set_max_level(level))
         .expect("Failed to set the logger");
-    Ok(&LOGGER)
+    logger
 }
 
 pub struct Logger {
-    log_targets: Arc<Mutex<Vec<&'static dyn LogTarget>>>
+    log_targets: Arc<Mutex<Vec<&'static dyn LogTarget>>>,
 }
 
 impl Logger {
@@ -23,10 +24,7 @@ impl Logger {
     }
 
     pub fn add_log_target(&self, log_target: &'static dyn LogTarget) {
-        self.log_targets
-            .lock()
-            .unwrap()
-            .push(log_target);
+        self.log_targets.lock().unwrap().push(log_target);
     }
 }
 
@@ -36,13 +34,9 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &Record) {
-        self.log_targets
-            .lock()
-            .unwrap()
-            .iter()
-            .for_each(|target| {
-                target.log(record);
-            });
+        self.log_targets.lock().unwrap().iter().for_each(|target| {
+            target.log(record);
+        });
     }
 
     fn flush(&self) {}
@@ -51,11 +45,10 @@ impl Log for Logger {
 impl Default for Logger {
     fn default() -> Self {
         Self {
-            log_targets: Arc::new(Mutex::new(vec![]))
+            log_targets: Arc::new(Mutex::new(vec![])),
         }
     }
 }
-
 
 pub trait LogTarget: Send + Sync {
     fn log(&self, record: &Record);
@@ -73,21 +66,14 @@ mod log_tests {
             static ref LOG_TARGET_A: TestLogTarget = TestLogTarget::new();
             static ref LOG_TARGET_B: TestLogTarget = TestLogTarget::new();
         }
-        let logger =
-            initialize_logging(LevelFilter::Trace).expect("Failed to initialize the logger");
+        let logger = initialize_logging(LevelFilter::Trace);
         logger.add_log_target(&*LOG_TARGET_A as &dyn LogTarget);
         logger.add_log_target(&*LOG_TARGET_B as &dyn LogTarget);
 
         info!("Hello, World!");
 
-        assert_eq!(
-            LOG_TARGET_B.last_message(),
-            "Hello, World!".to_string()
-        );
-        assert_eq!(
-            LOG_TARGET_B.last_message(),
-            "Hello, World!".to_string()
-        );
+        assert_eq!(LOG_TARGET_B.last_message(), "Hello, World!".to_string());
+        assert_eq!(LOG_TARGET_B.last_message(), "Hello, World!".to_string());
     }
 }
 
@@ -97,13 +83,13 @@ pub mod log_test_fixtures {
     use log::Record;
 
     pub struct TestLogTarget {
-        pub records: Arc<Mutex<Vec<String>>>
+        pub records: Arc<Mutex<Vec<String>>>,
     }
 
     impl TestLogTarget {
         pub fn new() -> Self {
             Self {
-                records: Arc::new(Mutex::new(vec![]))
+                records: Arc::new(Mutex::new(vec![])),
             }
         }
 
@@ -120,10 +106,7 @@ pub mod log_test_fixtures {
     impl LogTarget for TestLogTarget {
         fn log(&self, record: &Record) {
             let message = record.args().to_string();
-            self.records
-                .lock()
-                .unwrap()
-                .push(message);
+            self.records.lock().unwrap().push(message);
         }
     }
     unsafe impl Send for TestLogTarget {}
