51b277f1883eb85ccc11a55a8fb774bc15161996
AlexiWolf <alexiwolf@pm.me>
2021-08-14 22:44:40 -0500

    Added logging example

---
+++ examples/logging.rs
@@ -0,0 +1,13 @@
+use log::{trace, debug, info, warn, error, LevelFilter};
+use wolf_engine::logging::*;
+
+fn main() {
+    let logger = logger();
+    logger.set_log_level(LevelFilter::Debug);
+
+    trace!("Trace information.");
+    debug!("Debug information.");
+    info!("Normal information.");
+    warn!("Warning information");
+    error!("Critical information.");
+}
