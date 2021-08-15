d24809c0b7b658186e33c4a4d57a674b125ae35c
AlexiWolf <alexiwolf@pm.me>
2021-08-14 22:55:23 -0500

    Implemented more pleasing log format

--- src/logging/terminal_log_target.rs
+++ src/logging/terminal_log_target.rs
@@ -1,5 +1,8 @@
 use crate::logging::LogTarget;
-use log::Record;
+use log::{Record, Level};
+use chrono::Local;
+use colored::*;
+
 
 pub static TERMINAL_LOG_TARGET: TerminalLogTarget = TerminalLogTarget;
 
@@ -7,10 +10,19 @@ pub struct TerminalLogTarget;
 
 impl LogTarget for TerminalLogTarget {
     fn log(&self, record: &Record) {
+        let level_message;
+        match record.level() {
+            Level::Error => level_message = format!("{}", record.level()).red().bold(),
+            Level::Warn => level_message = format!("{}", record.level()).yellow().bold(),
+            Level::Info => level_message = format!("{}", record.level()).green().bold(),
+            Level::Debug => level_message = format!("{}", record.level()).blue().bold(),
+            Level::Trace => level_message = format!("{}", record.level()).white().bold(),
+        }
         println!(
-            "{} {}: {}",
-            record.level(),
-            record.module_path().unwrap_or("wolf_engine"),
+            "[{} {} {}] {}",
+            Local::now().format("%F %H:%M:%S"),
+            level_message,
+            record.module_path().unwrap_or(""),
             record.args()
         );
     }
