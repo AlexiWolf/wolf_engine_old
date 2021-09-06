3a61e7eae27bc5c600a240fc8c804a976f712c0a
AlexiWolf <alexiwolf@pm.me>
2021-09-06 14:49:52 -0500

    Send the correct values to the update function

--- src/core/game_loop/default_game_loop.rs
+++ src/core/game_loop/default_game_loop.rs
@@ -69,12 +69,12 @@ mod default_game_loop_test {
     fn should_have_consistent_tick_rate() {
         let mut last_update: Option<Instant> = None;
         let mut ticks_made = 0;
-        let context = Context;
-        let game_loop = DefaultGameLoopBuilder::new()
+        let mut context = Context;
+        let mut game_loop = DefaultGameLoopBuilder::new()
             .build();
 
         thead.sleep(Duration::from_millis(1000 / 30));
-        game_loop.update(context, |context| {
+        game_loop.update(&mut context, |context| {
             if let Some(last_update) = last_update {
                 assert_eq!(last_update.elapsed().as_millis(), 8);
             } else {
