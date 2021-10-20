2549f87f597aff131444f5ad7a4aa1d92e6259c6
AlexiWolf <alexiwolf@pm.me>
2021-10-20 05:58:59 -0500

    Test WolfEngineBuilder with default game loop

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -1,1 +1,23 @@
+
+/// Provides access to all engine state.
+/// 
+/// # Examples
+/// 
+/// Using the default game loop.
+/// 
+/// ```
+/// # use wolf_engine::WolfEngine;
+/// # use wolf_engine::FixedUpdateGameLoop
+/// 
+/// let engine: WolfEngine<FixedUpdateGameLoop> = WolfEngine::with_default_game_loop()
+///     .build();
+/// ```
 pub struct WolfEngine;
+
+pub struct WolfEngineBuilder;
+
+impl WolfEngineBuilder {
+    pub fn with_default_game_loop() -> Self {
+        Self
+    }
+}
