df9ab32f9bce3c55e96475c9f3aba13c30bf4b64
AlexiWolf <alexiwolf@pm.me>
2021-10-20 08:17:11 -0500

    Simple example of the engine

---
+++ examples/engine_initialization.rs
@@ -0,0 +1,31 @@
+use std::{sync::Arc, thread, time::Duration};
+
+use wolf_engine::WolfEngineBuilder;
+use log::info;
+
+pub fn main() {
+    let wolf_engine = WolfEngineBuilder::with_default_game_loop()
+        .build();
+    let _logging = wolf_engine::logging::logger();
+
+    let message = Arc::from(std::sync::Mutex::from("Hello, World!".to_string()));
+    let number = Arc::from(std::sync::Mutex::from(0 as u64));
+    
+    wolf_engine.run(
+        |_| {
+            let mut message = message.lock().unwrap();
+            let mut number = number.lock().unwrap();
+
+            *number += 1;
+            if *number % 2 == 0 {
+                *message = "Hello, World!".to_string();
+            } else {
+                *message = "Hello, from Wolf Engine!".to_string();
+            }
+        }, 
+        |_| {
+            info!("{}", message.lock().unwrap());
+            thread::sleep(Duration::from_secs_f64(1.0 / 120.0));
+        }
+    );
+}
