40681b16e9df059bef4edd6f9c15a9906497437c
AlexiWolf <alexiwolf@pm.me>
2021-10-20 22:49:40 -0500

    Updated the documentation

--- src/core/game_loop/game_loop_context.rs
+++ src/core/game_loop/game_loop_context.rs
@@ -5,12 +5,12 @@ use crate::{Frames, Ticks};
 /// Safely stores information about a [GameLoop](create::GameLoop).
 ///
 /// The main idea is the GameLoopInfo acts as a bridge between the [GameLoop](create::GameLoop), and the
-/// [GameLoopContext] instance.  It uses interior mutability with an [Arc], and a [Mutex] to safely allow the game loop
-/// to update the information while it's context reads from it.
+/// [GameLoopContext] instance.  It uses interior mutability to safely allow the game loop to update the information 
+/// while while the context reads from it.
 ///
 /// # Examples
 ///
-/// The game loop info object can be copied, but the copy will still have the same internal values as the other copies.
+/// The game loop info object can be immutably borrowed and shared freely.
 ///
 /// ```
 /// # use wolf_engine::GameLoopInfo;
