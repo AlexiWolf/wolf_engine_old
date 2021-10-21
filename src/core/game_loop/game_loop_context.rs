c1c425b9a6193525755a7674878dade76d2fbaf4
AlexiWolf <alexiwolf@pm.me>
2021-10-20 23:29:40 -0500

    Should have a from_game_loop constructor

--- src/core/game_loop/game_loop_context.rs
+++ src/core/game_loop/game_loop_context.rs
@@ -1,2 +1,13 @@
 /// Provides access to information and controls for the [GameLoop](crate::GameLoop).
+/// 
+/// # Examples
+/// 
+/// The only way to create a game loop context is from [GameLoop](crate::GameLoop) instance.
+/// 
+/// ```
+/// # use wolf_engine::{GameLoopContext, FixedUpdateGameLoopBuilder65};
+/// #
+/// # let game_loop = FixedUpdateGameLoopBuilder::new().build();
+/// let game_loop_context = GameLoopContext::from_loop(&game_loop);
+/// ```
 pub struct GameLoopContext;
