4b51507677153892468de0d1ec00c11a7d91e342
AlexiWolf <alexiwolf@pm.me>
2021-10-20 22:11:15 -0500

    Started copy test

--- src/core/game_loop/game_loop_context.rs
+++ src/core/game_loop/game_loop_context.rs
@@ -3,4 +3,14 @@
 /// The main idea is the GameLoopInfo acts as a bridge between the [GameLoop](create::GameLoop), and the
 /// [GameLoopContext] instance.  It uses interior mutability with an [Arc], and a [Mutex] to safely allow the game loop
 /// to update the information while it's context reads from it.
+/// 
+/// # Examples
+/// 
+/// The game loop info object can be copied, but the copy will still have the same internal values as the other copies.
+/// 
+/// ```
+/// # use wolf_engine::GameLoopInfo;
+/// #
+/// let info = GameLoopInfo::new();
+/// ```
 pub struct GameLoopInfo;
