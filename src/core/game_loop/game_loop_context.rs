3b8a0ce2c2cb421d8a56006390ac182475d42b9f
AlexiWolf <alexiwolf@pm.me>
2021-10-21 07:15:04 -0500

    Test the GameLoopContext exposes gameloop information

--- src/core/game_loop/game_loop_context.rs
+++ src/core/game_loop/game_loop_context.rs
@@ -10,7 +10,13 @@ use crate::GameLoopInfo;
 /// # use wolf_engine::{GameLoopContext, FixedUpdateGameLoopBuilder, GameLoop};
 /// #
 /// # let game_loop = FixedUpdateGameLoopBuilder::new().build();
+/// # game_loop.game_loop_info().add_tick();
+/// # game_loop.game_loop_info().add_frame();
+/// #
 /// let game_loop_context = GameLoopContext::from_game_loop_info(&game_loop.game_loop_info());
+/// 
+/// assert_eq!(game_loop_context.ticks(), 1);
+/// assert_eq!(game_loop_context.frames(), 1);
 /// ```
 pub struct GameLoopContext;
 
