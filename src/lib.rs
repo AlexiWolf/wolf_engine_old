ed33145ace3c524e0b671387b12bce297458d9a9
AlexiWolf <alexiwolf@pm.me>
2021-11-04 01:32:27 -0500

    Moved usage instructions to root module docs
    Users shouldn't have to dig for important usage documentation. It should
    be right on the front page.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,95 @@
+//! # Building an Engine Instance
+//!
+//! To initialize the engine, start by initializing a [Context] using the
+//! [ContextBuilder](crate::ContextBuilder).
+//!
+//! ```
+//! # use wolf_engine::ContextBuilder;
+//! #
+//! let context = ContextBuilder::new()
+//!     // Custom settings.
+//!     .build();
+//!```
+//!
+//! Then you can build and instance of the engine using the [WolfEngineBuilder].
+//! The `WolfEngineBuilder::with_default_game_loop()` method will give you the default
+//! [FixedUpdateGameLoop]. The default settings should be okay for most games.
+//!
+//! ```
+//! # use wolf_engine::{ContextBuilder, WolfEngineBuilder};
+//! #
+//! # let context = ContextBuilder::new()
+//! #    .build();
+//! #
+//! let engine = WolfEngineBuilder::with_default_game_loop()
+//!     .build(context);
+//! ```
+//!
+//! If you want to customize the [FixedUpdateGameLoop], you can build an instance yourself using the
+//! [FixedUpdateGameLoopBuilder](crate::game_loop::FixedUpdateGameLoopBuilder), then pass it to
+//! `WolfEngineBuilder::with_fixed_game_loop()`.
+//!
+//! ```
+//! # use wolf_engine::{ContextBuilder, WolfEngineBuilder, game_loop::FixedUpdateGameLoopBuilder};
+//! #
+//! # let context = ContextBuilder::new()
+//! #    .build();
+//! #
+//! let game_loop = FixedUpdateGameLoopBuilder::new()
+//!     // Custom settings.
+//!     .build();
+//!
+//! let engine = WolfEngineBuilder::with_fixed_game_loop(game_loop)
+//!     .build(context);
+//! ```
+//!
+//! ## Custom Game Loops
+//!  
+//! Alternatively, you can use a custom [GameLoop] implementation by using the
+//! `WolfEngineBuilder::with_custom_game_loop()` method.
+//!
+//! Refer to the [GameLoop] documentation for more information on implementing a custom game loop.
+//!
+//! ```
+//! # use wolf_engine::{ContextBuilder, WolfEngineBuilder, game_loop::{FixedUpdateGameLoopBuilder}};
+//! #
+//! # let context = ContextBuilder::new()
+//! #    .build();
+//! #
+//! # // For demonstrational purposes, this game loop will work, but it can be any GameLoop.
+//! # let custom_game_loop = FixedUpdateGameLoopBuilder::new()
+//! #   .build();
+//! #
+//! let engine = WolfEngineBuilder::with_custom_game_loop(custom_game_loop)
+//!     .build(context);
+//! ```
+//!
+//! # Starting the Engine
+//!  
+//! To run the engine, you provide the `update` and `render` functions for your game.  The engine
+//! will use the [GameLoop] to manage how the functions are called.  The engine will take ownership
+//! over itself and run until the game quits.
+//!
+//! ```
+//! # use wolf_engine::{WolfEngine, WolfEngineBuilder, ContextBuilder, game_loop::FixedUpdateGameLoop };
+//! #
+//! # let context = ContextBuilder::new()
+//! #    .build();
+//! #
+//! # let engine = WolfEngineBuilder::with_default_game_loop()
+//! #    .build(context);
+//! #
+//! engine.run(
+//!     |_context| {
+//!         // Update Function
+//!         # std::process::exit(0);
+//!     },
+//!     |_context| {
+//!         // Render Function
+//!     },
+//! );
+//! ```
+
 mod engine;
 
 pub mod context;
