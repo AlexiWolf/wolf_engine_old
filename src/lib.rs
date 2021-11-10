2ed65a6e8541cf87653087493b9d8022e3b7b7e1
AlexiWolf <alexiwolf@pm.me>
2021-11-09 19:45:17 -0600

    Removed the log module

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,6 @@ mod engine;
 
 pub mod context;
 pub mod game_loop;
-pub mod logging;
 
 pub use context::{Context, ContextBuilder};
 pub use engine::*;
--- src/logging/log_target.rs
+++
@@ -1,7 +0,0 @@
-use log::Record;
-
-/// Allows easy integration with the [Logger](crate::logging::Logger).
-pub trait LogTarget: Send + Sync {
-    /// Process / display the log message.
-    fn log(&self, record: &Record);
-}
