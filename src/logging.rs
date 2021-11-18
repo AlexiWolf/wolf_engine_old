3bb1e9028e1e956225d808fa0dde0d82097b1235
AlexiWolf <alexiwolf@pm.me>
2021-11-17 20:30:14 -0600

    Initalize the default terminal logger

--- src/logging.rs
+++ src/logging.rs
@@ -1,3 +1,10 @@
 use log::LevelFilter;
+use simple_logger::SimpleLogger;
 
-pub fn initialize_logging(log_level: LevelFilter) {}
+pub fn initialize_logging(log_level: LevelFilter) {
+    SimpleLogger::new()
+        .with_colors(true)
+        .with_level(log_level)
+        .init()
+        .expect("Failed to initialize the logger");
+}
