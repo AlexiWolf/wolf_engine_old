91dad43e774e21a95d4ceea85bfc65004ac2dd55
AlexiWolf <alexiwolf@pm.me>
2021-11-17 20:19:28 -0600

    Removed the comment from the example code
    I think this type of information is better to put in the documentation,
    rather than the code.  I'll probably get a second opinion on that later.

--- examples/logging.rs
+++ examples/logging.rs
@@ -2,7 +2,6 @@ use log::info;
 use wolf_engine::logging;
 
 pub fn main() {
-    // Wolf Engine provides terminal logging through the SimpleLogger crate.
     logging::initialize_logging();
     info!("Hello, world!");
 }
