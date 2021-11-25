7403b34f3676b7509417310bb9ffb14377bae4c9
= <alexiwolf@pm.me>
2021-11-24 20:34:32 -0600

    Rexport logging module in wolf_engine module

--- src/lib.rs
+++ src/lib.rs
@@ -96,10 +96,11 @@
 //! ```
 
 mod engine;
+mod logging;
 
 pub mod context;
 pub mod game_loop;
-pub mod logging;
 
 pub use context::{Context, ContextBuilder};
 pub use engine::*;
+pub use logging::*;
