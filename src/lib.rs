9824cdb75643b5e445bed951ff38b4fb0132a6bf
AlexiWolf <alexiwolf@pm.me>
2021-12-17 01:51:54 -0600

    Added state module

--- src/lib.rs
+++ src/lib.rs
@@ -97,6 +97,7 @@
 
 mod engine;
 mod logging;
+mod state;
 
 pub mod context;
 pub mod game_loop;
@@ -104,3 +105,4 @@ pub mod game_loop;
 pub use context::{Context, ContextBuilder};
 pub use engine::*;
 pub use logging::*;
+pub use state::*;
