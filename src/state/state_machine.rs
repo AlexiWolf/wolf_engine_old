dea0ec1cab9376efd06948286191e832e0a8fa5c
AlexiWolf <alexiwolf@pm.me>
2021-12-17 02:06:28 -0600

    Added an "empty construtor" test

--- src/state/state_machine.rs
+++ src/state/state_machine.rs
@@ -2,5 +2,15 @@ pub struct StateMachine;
 
 #[cfg(test)]
 mod state_machine_tests {
+    use super::*;
 
+    #[test]
+    pub fn should_have_an_empty_constructor() {
+        let state_machine = StateMachine::new_empty();
+        assert_eq!(
+            state_machine.stack.size(),
+            0,
+            "The state machine was initalized with a state on the stack"
+        );
+    }
 }
