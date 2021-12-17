fc276ec7ffa50faaebf17af3ad683c419147d59e
AlexiWolf <alexiwolf@pm.me>
2021-12-17 02:11:30 -0600

    Implemented StateMachine::new_empty constructor

--- src/state/state_machine.rs
+++ src/state/state_machine.rs
@@ -1,4 +1,16 @@
-pub struct StateMachine;
+use crate::State;
+
+pub struct StateMachine {
+    stack: Vec<Box<dyn State>>
+}
+
+impl StateMachine {
+    pub fn new_empty() -> Self {
+        Self {
+            stack: vec![] 
+        }
+    }
+}
 
 #[cfg(test)]
 mod state_machine_tests {
@@ -8,7 +20,7 @@ mod state_machine_tests {
     pub fn should_have_an_empty_constructor() {
         let state_machine = StateMachine::new_empty();
         assert_eq!(
-            state_machine.stack.size(),
+            state_machine.stack.len(),
             0,
             "The state machine was initalized with a state on the stack"
         );
