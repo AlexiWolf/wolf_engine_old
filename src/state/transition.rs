38e4693ea3dd1ee2ddf065e6bd84077ef6d596e5
AlexiWolf <alexiwolf@pm.me>
2021-12-31 16:31:14 -0600

    Provide full path the StateMachine link

--- src/state/transition.rs
+++ src/state/transition.rs
@@ -1,9 +1,10 @@
 use crate::State;
 
-/// Indicates if / how the [StateMachine] should change [State].
+/// Indicates if / how the [StateMachine](crate::StateMachine) should change [State].
 pub type Transition = Option<TransitionType>;
 
-/// Indicates the type of [Transition] the [StateMachine] should perform.
+/// Indicates the type of [Transition] the [StateMachine](crate::StateMachine) should 
+/// perform.
 pub enum TransitionType {
 
     /// Push a new [State] to the top of the stack.
