3db987e63c23b478b0f7183b8902857fdfbf4e68
AlexiWolf <alexiwolf@pm.me>
2021-12-31 00:47:19 -0600

    Added default Display implementation for States

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -12,9 +12,15 @@ pub type UpdateResult = Option<Transition>;
 
 pub type RenderResult = ();
 
-pub trait State: Display {
+pub trait State {
     fn update(&mut self, context: &mut Context) -> UpdateResult;
 
     fn render(&mut self, context: &mut Context) -> RenderResult;
+}
+
+impl Display for dyn State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
+    }
 }
 
--- src/state/state_machine.rs
+++ src/state/state_machine.rs
@@ -266,12 +266,5 @@ mod state_machine_tests {
 
             fn render(&mut self, _context: &mut Context) -> RenderResult {}
         }
-
-
-        impl Display for TransitionTestState {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "")
-            }
-        }
     }
 }
