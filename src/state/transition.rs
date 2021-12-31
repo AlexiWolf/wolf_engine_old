16e56c274da459ca5f6c8b749fe7dd3b8bf7583f
AlexiWolf <alexiwolf@pm.me>
2021-12-31 06:03:20 -0800

    Renamed Signal to TransitionType

--- src/state/transition.rs
+++ src/state/transition.rs
@@ -1,9 +1,9 @@
 use crate::State;
 
 /// Instructs the [StateMachine] on what to do.
-pub type Transition = Option<Signal>;
+pub type Transition = Option<TransitionType>;
 
-pub enum Signal {
+pub enum TransitionType {
     Push(Box<dyn State>),
     CleanPush(Box<dyn State>),
     Pop,
