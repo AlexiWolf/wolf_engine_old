40d268c356b4c4724adeb4aee39e53a3b9102dbb
AlexiWolf <alexiwolf@pm.me>
2022-01-15 07:31:03 -0800

    Updated documentation

--- src/state/transition.rs
+++ src/state/transition.rs
@@ -1,11 +1,12 @@
 use crate::State;
 
-/// Indicates if / how the [StateStack](crate::StateStack) should change [State].
+/// Indicates if a [Transition] should be performed.
 pub type OptionalTransition = Option<Transition>;
 
 /// Indicates the type of [Transition] the [StateStack](crate::StateStack) should
 /// perform.
 pub enum Transition {
+    /// Push a new [State] to the top of the stack.
     Push(Box<dyn State>),
 
     /// Pop the active [State] off the stack and shut it down.
