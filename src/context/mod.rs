eb5462ffbd8a81e6dcc514dd9300cb53182557b3
AlexiWolf <alexiwolf@pm.me>
2022-02-28 15:58:02 -0600

    Updated the Context documentation

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -7,12 +7,11 @@ pub use scheduler_context::*;
 #[cfg(feature = "window")]
 use winit::event_loop::EventLoop;
 
-/// Provides a central hub through which to access all other contexts.
+/// Provides access to global [Engine] state.
 ///
-/// This is the main context.  It may be helpful to think of it as the "gateway" to the whole engine
-/// because it owns all of the contexts active on the engine.  In many cases you will access
-/// specific contexts through the main context, but sometimes the main context will have helper
-/// functions for common tasks.
+/// The context object stores global state for the [Engine].  Any types that need to 
+/// work with the [Engine] can do so through the context object.  Most utility functions 
+/// will use the context object to do their work.
 ///
 /// # Examples
 ///
