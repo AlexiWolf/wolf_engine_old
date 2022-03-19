5449dce1b316a1e2a1c79a5a6843de1eaa0197bd
AlexiWolf <alexiwolf@pm.me>
2022-03-19 17:18:02 -0500

    Fixed broken test

--- src/contexts/event_context.rs
+++ src/contexts/event_context.rs
@@ -18,7 +18,7 @@ impl<E> EventContext<E> {
     }
 
     pub fn reader(&self) -> EventReader<E, DefaultSettings> {
-        EventReader::new(&*self.event_queue) 
+        EventReader::new(&self.event_queue) 
     }
 }
 
@@ -26,21 +26,21 @@ impl<E> EventContext<E> {
 mod event_context_tests {
     use std::fmt::Debug;
 
-    use rc_event_queue::LendingIterator;
+    use rc_event_queue::{LendingIterator, mpmc::DefaultSettings};
 
     pub use super::*;
     
     #[test]
     fn should_push_events_to_queue() {
         let events = EventContext::<u32>::new();
+        let mut reader = events.reader(); 
         
         events.push(1);
         
-        assert_next_event_equals(&events, &1);
+        assert_next_event_equals(&mut reader, &1);
     }
 
-    fn assert_next_event_equals<E: Eq + PartialEq + Debug>(events: &EventContext<E>, expected: &E) {
-        let mut reader = events.reader(); 
+    fn assert_next_event_equals<E: Eq + PartialEq + Debug>(reader: &mut EventReader<E, DefaultSettings>, expected: &E) {
         while let Some(event) = reader.iter().next() {
             assert_eq!(event, expected, "the events do not match");
             return
