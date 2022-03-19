c93e7025b9a966b1015afd6150166e9f394f9099
AlexiWolf <alexiwolf@pm.me>
2022-03-19 18:29:54 -0500

    Removed prelude, defined EventReaderSettings type

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,8 @@
 use rc_event_queue::mpmc::DefaultSettings;
 
-pub type EventQueue<E> = rc_event_queue::mpmc::EventQueue<E>;
-pub type EventReader<E> = rc_event_queue::mpmc::EventReader<E, DefaultSettings>;
+pub use rc_event_queue::LendingIterator;
 
-pub use rc_event_queue::prelude::*;
+pub type EventReaderSettings = DefaultSettings;
+pub type EventQueue<E> = rc_event_queue::mpmc::EventQueue<E>;
+pub type EventReader<E> = rc_event_queue::mpmc::EventReader<E, EventReaderSettings>;
 
