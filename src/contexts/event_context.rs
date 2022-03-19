a8ae823762247b283e6d3a013962075d27839a25
AlexiWolf <alexiwolf@pm.me>
2022-03-19 17:37:03 -0500

    Ran cargo fmt

--- src/contexts/event_context.rs
+++ src/contexts/event_context.rs
@@ -6,7 +6,7 @@ pub type EventQueue<E> = rc_event_queue::mpmc::EventQueue<E>;
 pub type EventReader<E> = rc_event_queue::mpmc::EventReader<E, DefaultSettings>;
 
 pub struct EventContext<E> {
-    event_queue: Pin<Arc<EventQueue<E>>> 
+    event_queue: Pin<Arc<EventQueue<E>>>,
 }
 
 impl<E> EventContext<E> {
@@ -17,11 +17,11 @@ impl<E> EventContext<E> {
     }
 
     pub fn push(&self, event: E) {
-        self.event_queue.push(event); 
+        self.event_queue.push(event);
     }
 
     pub fn reader(&self) -> EventReader<E> {
-        EventReader::new(&self.event_queue) 
+        EventReader::new(&self.event_queue)
     }
 }
 
@@ -32,21 +32,24 @@ mod event_context_tests {
     use rc_event_queue::LendingIterator;
 
     pub use super::*;
-    
+
     #[test]
     fn should_push_events_to_queue() {
         let events = EventContext::<u32>::new();
-        let mut reader = events.reader(); 
-        
+        let mut reader = events.reader();
+
         events.push(1);
-        
+
         assert_next_event_equals(&mut reader, &1);
     }
 
-    fn assert_next_event_equals<E: Eq + PartialEq + Debug>(reader: &mut EventReader<E>, expected: &E) {
+    fn assert_next_event_equals<E: Eq + PartialEq + Debug>(
+        reader: &mut EventReader<E>,
+        expected: &E,
+    ) {
         while let Some(event) = reader.iter().next() {
             assert_eq!(event, expected, "the events do not match");
-            return
+            return;
         }
         panic!("No events received")
     }
--- src/contexts/mod.rs
+++ src/contexts/mod.rs
@@ -1,8 +1,7 @@
 //! Provides built-in [Subcontext](crate::Subcontext) implementations.
 
-mod scheduler_context;
 mod event_context;
+mod scheduler_context;
 
-pub use scheduler_context::*;
 pub use event_context::*;
-
+pub use scheduler_context::*;
