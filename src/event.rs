a24a85e7fe610d0c34d0810fdb7eb9a66ab757f3
AlexiWolf <alexiwolf@pm.me>
2022-03-19 20:11:47 -0500

    Reexported rc_event_queue

--- src/event.rs
+++ src/event.rs
@@ -1,8 +1,15 @@
-use rc_event_queue::mpmc::DefaultSettings;
+//! Provides default engine events and an event system backed by [rc_event_queue].
 
-pub use rc_event_queue::LendingIterator;
+pub mod rc_event_queue {
+    //! Reexports [rc_event_queue]. 
+    pub use rc_event_queue::*;
+}
+
+use ::rc_event_queue::mpmc::DefaultSettings;
+
+pub use ::rc_event_queue::LendingIterator;
 
 pub type EventReaderSettings = DefaultSettings;
-pub type EventQueue<E> = rc_event_queue::mpmc::EventQueue<E>;
-pub type EventReader<E> = rc_event_queue::mpmc::EventReader<E, EventReaderSettings>;
+pub type EventQueue<E> = ::rc_event_queue::mpmc::EventQueue<E>;
+pub type EventReader<E> = ::rc_event_queue::mpmc::EventReader<E, EventReaderSettings>;
 
