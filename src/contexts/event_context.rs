28cac0228e9e09c7393b6a8c0308b9d867167b87
AlexiWolf <alexiwolf@pm.me>
2022-03-21 02:40:38 -0500

    Documented EventContext

--- src/contexts/event_context.rs
+++ src/contexts/event_context.rs
@@ -5,6 +5,118 @@ use crate::{
     Subcontext,
 };
 
+/// Provides an event system usable through the [Context](crate::Context).
+///
+/// The Event Context is a small wrapper around [rc_event_queue]s API.  It helps to 
+/// integrate everything more cleanly into the engine and is a bit more friendly than 
+/// using [EventQueue] and [EventReader] directly.
+///
+/// # Examples
+///
+/// To create a new Event Context, you should use [EventContext::default()].  You must 
+/// provide an event type parameter, or a type annotation to indicate which type you want
+/// to use an event.  Most types can be used as an Event.
+///
+/// ```
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event; 
+/// #
+/// let event_context = EventContext::<Event>::default();
+/// ```
+/// Or, alternatively:
+///
+/// ```
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event; 
+/// #
+/// let event_context: EventContext<Event> = EventContext::default();
+/// ```
+/// 
+/// Pushing events is done using [EventContext::push()] like so:
+///
+/// ```
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event; 
+/// #
+/// # let event_context = EventContext::<usize>::default();
+/// # let event = 0;
+/// #
+/// event_context.push(event);
+/// ```
+///
+/// Processing events is done through an [EventReader].  You can get an [EventReader]
+/// using [EventContext::reader()].
+///
+/// ```
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event; 
+/// #
+/// # let event_context = EventContext::<usize>::default();
+/// #
+/// let mut event_reader = event_context.reader();
+/// ```
+///
+/// ```
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::*;
+/// #
+/// # let event_context = EventContext::<usize>::default();
+/// # let mut event_reader = event_context.reader();
+/// #
+/// while let Some(event) = event_reader.iter().next() {
+///     // Process the event.
+/// }
+/// ```
+///
+/// In most cases, you're not going to be using an Event Context in isolation.  You'll
+/// probably be working with an existing Event Context stored on the 
+/// [Context](crate::Context).  You can access specific Event Contexts through the 
+/// [Context::get()](crate::Context) method.
+///
+/// **Note:** There are no methods on the Event Context that require a mutable reference, 
+/// so make sure you use [Context::get()](crate::Context) for an immutable reference.
+///
+/// ```
+/// # use wolf_engine::Context;
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event; 
+/// #
+/// # let mut context = Context::empty();
+/// # context.add(EventContext::<Event>::default()).unwrap();
+/// #
+/// let event_context = context.get::<EventContext<Event>>().expect("no event context");
+/// ```
+///
+/// # Preventing Memory Leaks
+///
+/// One of the main problems with using [EventQueue] and [EventReader] directly, is it's 
+/// possible to trigger memory leaks if an [EventReader] is created, but not read from.
+/// The [EventQueue] does not drop events unless all of its [EventReader]s have read the
+/// event.  This allows a rogue [EventReader] to cause events to stack up, resulting in 
+/// a memory leak.
+///
+/// The Event Context helps to prevent this issue by imposing a limit on how many events
+/// the [EventQueue] can contain.  If this limit is exceeded, the oldest events will be 
+/// forcibly removed and dropped.  While this means some events may never be processed, 
+/// in most cases this is preferable to crashing.
+///
+/// The best way to ensure the event cap is not reached is to ensure all [EventReader]s 
+/// are being read from, and are dropped when you are done with them.
+///
+/// ## Increasing the Queue Size Limit 
+///
+/// The default event cap is about 100K, and in most cases (assuming your game is working
+/// normally), you shouldn't get anywhere near the limit.  If you find the cap isn't 
+/// enough for your game, the cap can be increased using [EventContext::new()].
+///
+/// ```Rust
+/// # use wolf_engine::contexts::EventContext;
+/// # use wolf_engine::event::Event;
+/// #
+/// # let custom_queue_limit = 20_000;
+/// #
+/// let event_context = EventContext::<Event>::new(custom_queue_limit);
+/// ```
 pub struct EventContext<E> {
     event_queue: Pin<Arc<EventQueue<E>>>,
     pub max_queue_size: usize,
