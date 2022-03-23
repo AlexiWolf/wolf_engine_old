ddd47693c1b2e14302d7a042edf0a1bc18c03efe
AlexiWolf <alexiwolf@pm.me>
2022-03-23 11:07:34 -0500

    Added EngineBuilder return type to Plugin::setup()

--- src/core/plugin.rs
+++ src/core/plugin.rs
@@ -5,6 +5,6 @@ use crate::EngineBuilder;
 
 #[cfg_attr(test, automock)]
 pub trait Plugin {
-    fn setup(&mut self, engine_builder: EngineBuilder);
+    fn setup(&mut self, engine_builder: EngineBuilder) -> EngineBuilder;
 }
 
