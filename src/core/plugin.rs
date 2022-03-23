26433323da6b83c5f34c9b94cdc1bc85671b9cb1
AlexiWolf <alexiwolf@pm.me>
2022-03-23 10:55:21 -0500

    Added setup method to Plugin

--- src/core/plugin.rs
+++ src/core/plugin.rs
@@ -1,6 +1,10 @@
 #[cfg(test)]
 use mockall::automock;
 
+use crate::EngineBuilder;
+
 #[cfg_attr(test, automock)]
-pub trait Plugin {}
+pub trait Plugin {
+    fn setup(&mut self, engine_builder: EngineBuilder);
+}
 
