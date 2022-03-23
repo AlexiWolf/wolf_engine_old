e4100bd3184744b5372848a8df70e9d69f67905f
AlexiWolf <alexiwolf@pm.me>
2022-03-23 12:15:17 -0500

    Implemented CorePlugin

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,9 +1,11 @@
-use crate::{EngineBuilder, Plugin};
+use crate::{EngineBuilder, Plugin, contexts::{SchedulerContext, EventContext}, event::Event};
 
 pub(crate) struct CorePlugin; 
 
 impl Plugin for CorePlugin {
     fn setup(&mut self, engine_builder: EngineBuilder) -> EngineBuilder {
         engine_builder
+            .with_subcontext(SchedulerContext::new())
+            .with_subcontext(EventContext::<Event>::default())
     }
 }
