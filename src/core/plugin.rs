52f55bccf5b1163409681b476518d1270337678c
AlexiWolf <alexiwolf@pm.me>
2022-03-23 22:05:53 -0500

    Ran cargo fmt

--- src/core/plugin.rs
+++ src/core/plugin.rs
@@ -6,17 +6,17 @@ use crate::EngineBuilder;
 /// Provides additional functionality to the engine.
 ///
 /// Plugins make it easy to extend the engine with new functionality.  Plugins are loaded
-/// at startup by the [EngineBuilder].  Plugins are given direct access to the 
-/// [EngineBuilder] during startup, and can add to it, or configure it in any way they 
+/// at startup by the [EngineBuilder].  Plugins are given direct access to the
+/// [EngineBuilder] during startup, and can add to it, or configure it in any way they
 /// need.
 ///
-/// Among other things, most plugins will: 
+/// Among other things, most plugins will:
 ///
 /// - Add `Subcontext`s to the main `Context`.
 /// - Remove `Subcontext`s from the main `Context`.
 /// - Add other `Plugins`.
 /// - Change the engine's `Scheduler`.
-/// - Change the engine's `CoreFunction`. 
+/// - Change the engine's `CoreFunction`.
 ///
 /// # Examples
 ///
@@ -40,7 +40,7 @@ use crate::EngineBuilder;
 /// ## Creating a Custom Plugin
 ///
 /// You can create a custom plugin by implementing this trait.
-/// 
+///
 /// ```
 /// # use wolf_engine::*;
 /// #
