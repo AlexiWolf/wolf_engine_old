8019be58623ab003d1688f9b384246f672b4f68b
AlexiWolf <alexiwolf@pm.me>
2022-03-23 22:05:34 -0500

    Added Plugin usage example

---
+++ examples/plugins.rs
@@ -0,0 +1,54 @@
+use log::*;
+use wolf_engine::*;
+
+pub fn main() {
+    #[cfg(feature = "logging")]
+    logging::initialize_logging(LevelFilter::Info);
+
+    EngineBuilder::new()
+        .with_plugin(Box::from(MessagePlugin::new("Hello, world!")))
+        .build()
+        .run(Box::from(GameState));
+}
+
+pub struct MessagePlugin {
+    message: String,
+}
+
+impl Plugin for MessagePlugin {
+    fn setup(&mut self, engine_builder: EngineBuilder) -> EngineBuilder {
+        engine_builder.with_subcontext(MessageContext::new(self.message.clone()))
+    }
+}
+
+impl MessagePlugin {
+    pub fn new(message: &str) -> Self {
+        Self {
+            message: message.to_string(),
+        }
+    }
+}
+
+pub struct MessageContext {
+    message: String,
+}
+
+impl MessageContext {
+    pub fn new(message: String) -> Self {
+        Self { message }
+    }
+}
+
+impl Subcontext for MessageContext {}
+
+pub struct GameState;
+
+impl State for GameState {
+    fn update(&mut self, context: &mut Context) -> OptionalTransition {
+        let message = context.get::<MessageContext>().unwrap();
+        info!("{}", message.message);
+        Some(Transition::Quit)
+    }
+
+    fn render(&mut self, _context: &mut Context) -> RenderResult {}
+}
