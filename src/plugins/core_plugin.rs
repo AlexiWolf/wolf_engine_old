c79912ef8e8f9b3231368e751c542c22730481ec
AlexiWolf <alexiwolf@pm.me>
2022-03-23 21:45:11 -0500

    Documented CorePlugin

--- src/plugins/core_plugin.rs
+++ src/plugins/core_plugin.rs
@@ -2,6 +2,7 @@ use crate::contexts::{EventContext, SchedulerContext};
 use crate::event::Event;
 use crate::*;
 
+/// Provides core functionality that **must** be loaded in order for the engine to work.
 pub(crate) struct CorePlugin;
 
 impl Plugin for CorePlugin {
