bbc6d0fc87c5110b9ec93e68053705b913e21e9d
AlexiWolf <alexiwolf@pm.me>
2022-04-07 11:23:52 -0500

    Updated engine test to return PluginResult

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -240,7 +240,7 @@ mod engine_builder_tests {
         plugin
             .expect_setup()
             .times(1)
-            .returning(|engine_builder| engine_builder);
+            .returning(|engine_builder| Ok(engine_builder));
 
         let _engine = EngineBuilder::new().with_plugin(Box::from(plugin)).build();
     }
