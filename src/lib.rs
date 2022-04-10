ae9df782450533a234d68894fd48ed9e3da787f5
AlexiWolf <alexiwolf@pm.me>
2022-04-10 09:56:24 -0500

    Added draft for engine guide

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@
 //! # Getting Started
 //!
 //! Wolf Engine ships with sensible defaults to help you jump-start projects as quickly 
-//! as possible.  Using `Engine::new()` will initialize the engine with the default 
-//! settings.
-//!
+//! as possible.  To get started with the default settings, use [Engine::new()] and 
+//! provide your game's starting [State] to the [Engine::run()] method.
 //! ```
 //! pub use wolf_engine::*;
 //! 
@@ -13,7 +12,7 @@
 //!     Engine::new()
 //!         .run(Box::from(MyGameState::new()));
 //! }
-//!
+//! 
 //! pub struct MyGameState {
 //!     number: usize,
 //! }
@@ -45,6 +44,49 @@
 //!
 //! More complete examples can be found in the 
 //! [Examples Folder](https://github.com/AlexiWolf/wolf_engine/tree/main/examples).
+//!
+//! # Game States
+//!
+//! Wolf Engine games are implemented as a collection of [State] objects.  Each [State] 
+//! holds some data and logic for a specific part of the game.  The main use for [State]s
+//! is to help you split your game into manageable pieces.
+//!
+//! For example, you may have a game with several parts: 
+//!
+//! - A main menu screen. 
+//! - A level / game-play screen.
+//! - A "game over" screen.
+//!
+//! Instead of trying to shove all these behaviors into a single place, you could divide 
+//! your game into several states instead:
+//!
+//! - `MainMenuState`: Displays the game's logo and has a play button.
+//! - `GameplayState`: Runs the main game logic.  
+//! - `GameOverState`: Shows the player's final score, and prompts them to play again.
+//!
+//! ## The State Stack
+//!
+//! The [StateStack] is the main organizational component of the [Engine].  It allows for 
+//! [State] objects to be stacked on top of each other, and allows for "layered" game 
+//! logic.  The state on the top of the stack is designated the `active` [State], and all 
+//! other [State]s are designated as `inactive.`  The active [State] assumes control, 
+//! while inactive [State]s continue to run the background.
+//!
+//! A good example of why this system is useful is when implementing a pause menu for your
+//! game.  When the player pauses the game:  
+//!
+//! 1. The `PausedState` is pushed to the stack, and it takes over as the `active` state.
+//! 2. When the player is done in the pause menu, the `PausedState` is popped off the 
+//!    stack, and the `GameState` becomes `active` again and is resumed. 
+//!
+//! A very useful effect of layered game logic is it's possible for inactive [State]s to 
+//! continue running in the background.  For example, if your game is multi-player, the 
+//! world can continue running even though the client is "paused."  In other cases, such
+//! as if your pause menu is transparent, you may want to stop game logic from updating,
+//! but continue running animations and effects in the background.
+//!
+//! ## State Transitions
+//!
 mod core;
 
 pub mod contexts;
