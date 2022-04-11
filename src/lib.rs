47ce60804a6b45712d41c8f63f215fe09191f8ce
AlexiWolf <alexiwolf@pm.me>
2022-04-11 09:06:45 -0500

    Added documentation about the Context object

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,79 @@
 //!
 //! More complete examples can be found in the 
 //! [Examples Folder](https://github.com/AlexiWolf/wolf_engine/tree/main/examples).
+//!
+//! # Context Data
+//!
+//! [Engine] data is stored in the [Context].  The [Context] is a dynamic storage 
+//! container which allows us to add new data to the [Engine] at run-time.
+//!
+//! If you have access to the [Context], you can request access to stored data by it's 
+//! type.  Rust's normal borrowing rules still apply, but they are checked at run-time not 
+//! compile-time.  
+//!
+//! ```
+//!# pub use wolf_engine::*;
+//!# 
+//!# pub fn main() {
+//!#     Engine::new()
+//!#         .run(Box::from(MyGameState));
+//!# }
+//!# 
+//!# pub struct MyGameState;
+//!#
+//!# impl State for MyGameState {
+//! fn update(&mut self, context: &mut Context) -> OptionalTransition {
+//!     if let Some(Ok(subcontext)) = context.try_borrow::<ExampleContext>() {
+//!         log::info!("{}", subcontext.message);     
+//!         // This subcontext must go out of scope before we can borrow mutably.
+//!     }
+//!     if let Some(Ok(mut subcontext)) = context.try_borrow_mut::<ExampleContext>() {
+//!         // If anything else is borrowing the subcontext, we will get an error.
+//!         subcontext.message = "New Message".to_string();
+//!         log::info!("{}", subcontext.message);
+//!     }
+//!#    return Some(Transition::Quit);
+//!     None
+//! }
+//!#
+//!#     fn render(&mut self, _context: &mut Context) -> RenderResult {}
+//!# }
+//!# 
+//!# pub struct ExampleContext {
+//!#    pub message: String,  
+//!# } 
+//!#
+//!# impl Subcontext for ExampleContext {}
+//! ```
+//!
+//! It is best to use [Context::try_borrow()] and [Context::try_borrow_mut()] instead of 
+//! [Context::borrow()] and [Context::borrow_mut()], as the non-try methods will panic if 
+//! the borrowing rules are broken.
+//!
+//! ## Functions Using the Context 
+//!
+//! A common pattern for Wolf Engine is passing the [Context] or specific [Subcontext] 
+//! objects to functions implementing some functionality.  Because the [Context] grants
+//! access to all [Engine] data, functions can use it to work on the current instance of 
+//! the [Engine].
+//! 
+//! ```
+//!# use wolf_engine::*;
+//!#
+//!# let context = &Context::new();
+//!# fn some_function(context: &Context) {}
+//! some_function(context);
+//! ```
+//!
+//! Implementing new functions using this pattern is also easy.
+//!
+//! ```
+//! # use wolf_engine::*;
+//! #
+//! pub fn my_function(context: &Context) {
+//!     // Do something cool.
+//! }
+//! ```
 mod core;
 
 pub mod contexts;
