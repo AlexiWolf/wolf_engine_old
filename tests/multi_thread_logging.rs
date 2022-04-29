2e5eaa9a5a297c61e338ea2b6c451a559e6dfa74
AlexiWolf <alexiwolf@pm.me>
2022-04-28 20:07:23 -0500

    Ran cargo fmt

--- tests/multi_thread_logging.rs
+++ tests/multi_thread_logging.rs
@@ -1,16 +1,16 @@
 #[cfg(test)]
 #[cfg(feature = "logging")]
 pub mod multi_threaded_logging_tests {
-    use wolf_engine::*;
     use log::*;
     use std::thread;
+    use wolf_engine::*;
 
     #[test]
     pub fn should_not_panic_in_multi_threaded_environment() {
-        logging::initialize_logging(LevelFilter::Info); 
+        logging::initialize_logging(LevelFilter::Info);
 
         let thread = thread::spawn(|| {
-            info!("Hello, world!"); 
+            info!("Hello, world!");
         });
         thread.join().unwrap();
     }
