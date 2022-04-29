7239ffce78e4f62362c12df311242e3f6cabb114
AlexiWolf <alexiwolf@pm.me>
2022-04-29 02:46:33 -0500

    Implemented PuffinHttpContext setup

--- src/contexts/puffin_http_context.rs
+++ src/contexts/puffin_http_context.rs
@@ -1,11 +1,24 @@
+use puffin_http::{Server, Client};
+
 use crate::*;
 
-pub struct PuffinHttpContext {}
+pub struct PuffinHttpContext {
+    pub server: Server,
+    _client: Client,
+}
 
 impl PuffinHttpContext {
     pub fn new(server_address: &str) -> Result<Self, ()> {
-        let http_context = Self {};
-        Ok(http_context)
+        let server_result = Server::new(server_address);
+        if server_result.is_ok() {
+            let client = Client::new("127.0.0.1:8585".to_owned());
+            Ok(Self {
+                server: server_result.unwrap(),
+                _client: client
+            })
+        } else {
+            Err(server_result.err().unwrap())
+        }
     }
 }
 
