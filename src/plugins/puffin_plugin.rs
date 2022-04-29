6c66e7a6d04b296c5d64e476776c120e5a460edb
AlexiWolf <alexiwolf@pm.me>
2022-04-29 02:24:08 -0500

    Enabled profiling when PuffinPlugin is loaded

--- src/plugins/puffin_plugin.rs
+++ src/plugins/puffin_plugin.rs
@@ -5,7 +5,7 @@ pub struct PuffinPlugin;
 
 impl Plugin for PuffinPlugin {
     fn setup(&mut self,engine_builder:EngineBuilder) -> PluginResult {
+        puffin::set_scopes_on(true);
         Ok(engine_builder)
     }
-
 }
