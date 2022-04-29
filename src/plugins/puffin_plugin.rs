b2ad55408c2aedf592991944cdf107d250030305
AlexiWolf <alexiwolf@pm.me>
2022-04-29 02:57:12 -0500

    Implemented puffin http server setup

--- src/plugins/puffin_plugin.rs
+++ src/plugins/puffin_plugin.rs
@@ -1,17 +1,29 @@
 use crate::*;
+use log::*;
 
 /// Provides profiling using [puffin].
 pub struct PuffinPlugin;
 
 impl Plugin for PuffinPlugin {
-    fn setup(&mut self,engine_builder:EngineBuilder) -> PluginResult {
+    fn setup(&mut self, mut engine_builder: EngineBuilder) -> PluginResult {
         if cfg!(feature = "profiling") {
             puffin::set_scopes_on(true);
         }
-        if cfg!(feature = "http_profiling") {
-            use contexts::PuffinHttpContext;
-            let puffin_server_result = PuffinHttpContext::new();
-        }
+        #[cfg(feature = "http_profiling")]
+        {engine_builder = enable_puffin_http(engine_builder);}
         Ok(engine_builder)
     }
+}
+
+#[cfg(feature = "http_profiling")]
+fn enable_puffin_http(mut engine_builder: EngineBuilder) -> EngineBuilder {
+    let puffin_server_result = contexts::PuffinHttpContext::new();
+    if puffin_server_result.is_ok() {
+        let http_context = puffin_server_result.unwrap();
+        info!("Successfully created Puffin HTTP server at: 0.0.0.0:8585");
+        engine_builder = engine_builder.with_subcontext(http_context);
+    } else {
+        warn!("Failed to create Puffin HTTP server at: 0.0.0.0:8585: {}", puffin_server_result.err().unwrap());
+    }
+    engine_builder
 }
