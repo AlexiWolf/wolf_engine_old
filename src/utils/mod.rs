b7866a45c9aa61d599fd6d43fa82f2ac9a2e1f4b
AlexiWolf <alexiwolf@pm.me>
2022-04-30 05:43:14 -0500

    Added profile_new_frame() function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,3 +3,10 @@
 pub mod trust_cell;
 
 pub use puffin::{current_file_name, current_function_name, profile_function, profile_scope};
+
+/// Start a new [puffin] frame.
+///
+/// Equivalent to calling [puffin::GlobalProfiler::new_frame()].
+pub fn profile_new_frame() {
+    puffin::GlobalProfiler::lock().new_frame(); 
+}
