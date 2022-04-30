c9932a9cc3012aa2e87f1c21c78dd71ac51a973d
AlexiWolf <alexiwolf@pm.me>
2022-04-30 05:46:57 -0500

    Added usage example doctest

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,6 +7,19 @@ pub use puffin::{current_file_name, current_function_name, profile_function, pro
 /// Start a new [puffin] frame.
 ///
 /// Equivalent to calling [puffin::GlobalProfiler::new_frame()].
+///
+/// # Examples
+///
+/// ```
+/// # use wolf_engine::utils::*;
+/// #
+/// loop {
+///     profile_new_frame(); // Start the new frame.
+///     profile_scope!("frame"); // Start the frame scope.
+///     // Update / Render / cool stuff.
+/// #   break;
+/// }
+/// ```
 pub fn profile_new_frame() {
     puffin::GlobalProfiler::lock().new_frame(); 
 }
