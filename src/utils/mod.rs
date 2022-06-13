5d765881368a3535e685222db1083e2bf82493ef
AlexiWolf <alexiwolf@pm.me>
2022-06-13 04:04:31 -0500

    Removed now unused `TrustCell` code from the project

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,5 @@
 //! Provides common helpers and utilities.
 
-pub mod trust_cell;
-
 pub use puffin::{current_file_name, current_function_name, profile_function, profile_scope};
 
 /// Start a new [puffin] frame.
