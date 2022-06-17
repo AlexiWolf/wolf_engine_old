32df1c63b879a128392af0220c5cfaa54cc6a18e
AlexiWolf <alexiwolf@pm.me>
2022-06-16 21:06:16 -0500

    Updated the `CorePlugin` to add an `EngineContext`

--- src/plugins/core_plugin.rs
+++ src/plugins/core_plugin.rs
@@ -1,4 +1,4 @@
-use crate::contexts::{EventContext, SchedulerContext};
+use crate::contexts::{EventContext, SchedulerContext, EngineContext, EngineContext};
 use crate::event::Event;
 use crate::*;
 
@@ -8,6 +8,7 @@ pub(crate) struct CorePlugin;
 impl Plugin for CorePlugin {
     fn setup(&mut self, engine_builder: EngineBuilder) -> PluginResult {
         Ok(engine_builder
+            .with_subcontext(EngineContext::new())
             .with_subcontext(SchedulerContext::new())
             .with_subcontext(EventContext::<Event>::default()))
     }
