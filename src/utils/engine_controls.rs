a03221f117849df92b357668125ff75ef1f28912
AlexiWolf <alexiwolf@pm.me>
2022-06-16 20:58:56 -0500

    Stubbed `EngineControls` implementation for `Context`

--- src/utils/engine_controls.rs
+++ src/utils/engine_controls.rs
@@ -1,16 +1,23 @@
+use crate::*;
+
 pub trait EngineControls {
     fn quit(&mut self);
+}
+
+impl EngineControls for Context {
+    fn quit(&mut self) {
+        
+    }
 }
 
 #[cfg(test)]
 mod engine_controls_test {
     use super::*;
-    use crate::*;
 
     #[test]
     fn should_quit() {
         let mut engine = Engine::default();
-
+        
         engine.context.quit();
          
         assert!(!engine.is_running(), "The engine is running, but it should not be.");
