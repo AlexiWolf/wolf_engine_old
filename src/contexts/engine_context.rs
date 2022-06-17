3aedccd7d7e488b7a0f578db2006d1d90ebafe01
AlexiWolf <alexiwolf@pm.me>
2022-06-16 20:46:30 -0500

    Removed `pub` from test function

--- src/contexts/engine_context.rs
+++ src/contexts/engine_context.rs
@@ -19,7 +19,7 @@ mod engine_context_tests {
     use super::*;
     
     #[test]
-    pub fn should_initialize_has_quit_to_false() {
+    fn should_initialize_has_quit_to_false() {
         let engine_context = EngineContext::new();
 
         assert!(!engine_context.has_quit, "has_quit should be false.");
