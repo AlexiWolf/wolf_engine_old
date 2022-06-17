bad22983784b5c231c0b935c38df915cc6ba96b3
AlexiWolf <alexiwolf@pm.me>
2022-06-16 20:43:21 -0500

    Changed to `use super::*`

--- src/contexts/engine_context.rs
+++ src/contexts/engine_context.rs
@@ -6,9 +6,7 @@ impl Subcontext for EngineContext {}
 
 #[cfg(test)]
 mod engine_context_tests {
-    use super::EngineContext;
-
-    use super::EngineContext;
+    use super::*;
 
     pub fn should_initialize_has_quit_to_false() {
         let engine_context = EngineContext::new();
