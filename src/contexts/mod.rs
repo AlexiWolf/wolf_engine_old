b5f619a0505aad4dd1e528bfec7754500dda28a8
AlexiWolf <alexiwolf@pm.me>
2022-06-16 20:19:39 -0500

    Created engine_context module

--- src/contexts/mod.rs
+++ src/contexts/mod.rs
@@ -1,10 +1,12 @@
 //! Provides built-in [Subcontext](crate::Subcontext) implementations.
 
+mod engine_context;
 mod event_context;
 #[cfg(feature = "http_profiling")]
 mod puffin_http_context;
 mod scheduler_context;
 
+pub use engine_context::*;
 pub use event_context::*;
 #[cfg(feature = "http_profiling")]
 pub use puffin_http_context::*;
