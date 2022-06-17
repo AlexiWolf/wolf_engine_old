ca59fe68c3ed9f480f46fe4e15be171939a91c52
AlexiWolf <alexiwolf@pm.me>
2022-06-16 20:42:41 -0500

    Added test of EngineContext initialization

--- src/contexts/engine_context.rs
+++ src/contexts/engine_context.rs
@@ -3,3 +3,16 @@ use crate::*;
 pub struct EngineContext {}
 
 impl Subcontext for EngineContext {}
+
+#[cfg(test)]
+mod engine_context_tests {
+    use super::EngineContext;
+
+    use super::EngineContext;
+
+    pub fn should_initialize_has_quit_to_false() {
+        let engine_context = EngineContext::new();
+
+        assert!(!engine_context.has_quit, "has_quit should be false.");
+    }
+}
