7b78cb097d62b0f0ab3bbe252a73a8a6546455a1
AlexiWolf <alexiwolf@pm.me>
2022-06-18 16:01:43 -0500

    Updated main loop example comments

--- examples/custom_engine_core.rs
+++ examples/custom_engine_core.rs
@@ -6,19 +6,16 @@ pub fn main() {
     logging::initialize_logging(LevelFilter::Info);
 
     EngineBuilder::new()
-        .with_engine_core(Box::from(my_custom_core_function))
+        .with_engine_core(Box::from(MyMainLoop))
         .build()
         .expect("Failed to build the engine")
         .run(Box::from(EmptyState));
 }
 
-/// Engine cores are just normal functions.
+/// A main loop is any struct which implements the `MainLoop` trait.
 ///
-/// The only requirements are they take an [Engine] as an argument, and they don't
-/// return anything.
-///
-/// Core functions offer you a lot of control because they give you full ownership over
-/// the running [Engine] instance.  This essentially means you can directly control over
+/// Main loops offer you a lot of control because they give you full ownership over
+/// the running `Engine` instance.  This essentially means you can directly control
 /// how the engine runs things.
 pub struct MyMainLoop;
 
@@ -26,7 +23,7 @@ impl MainLoop for MyMainLoop {
     fn run(&mut self, mut engine: Engine) -> Engine {
         info!("Hello, from a custom core function!");
 
-        // Lets make a simple core that runs 10 times, the quits.
+        // Lets make a simple main loop that runs 10 times, the quits.
         let mut iterations = 0;
 
         while iterations > 0 {
