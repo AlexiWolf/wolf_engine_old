a8b57a00f4a7d49813c5cf7a9ff5f25ebcaaa5bf
AlexiWolf <alexiwolf@pm.me>
2022-06-18 15:10:15 -0500

    Updated and revised the `MainLoop` documentation

--- src/core/main_loop.rs
+++ src/core/main_loop.rs
@@ -4,31 +4,32 @@ use crate::Engine;
 
 /// Defines which functions can be used as an [Engine] core.
 ///
-/// Core functions take ownership over the running [Engine], and directly implement the
-/// main-loop for the [Engine].  When [Engine::run()] is called, the main-loop behavior
-/// is delegated to the core function provided at startup.
+/// Main loops take ownership over the running [Engine], and directly implement the
+/// main loop for the [Engine].  When [Engine::run()] is called, the main loop behavior
+/// is delegated to the main loop implementation provided at startup.
 ///
-/// The main reason for separating the core main-loop from the [Engine] to make it easy
+/// The main reason for separating the core main loop from the [Engine] to make it easy
 /// to change the [Engine]'s core behavior without needing to rewrite its code.  Using a
-/// core function, you could, for example:
+/// a main loop, you could, for example:
 ///
 /// - Change the behavior of the main loop to better suit your game's needs.
-/// - Integrate with 3rd party frameworks (such as Winit, Calloop, or Tokio), and allow
+/// - Integrate with 3rd party frameworks (such as Winit, SDL, or Tokio), and allow
 ///   them to control the main loop.
-/// - Extend existing engine cores with useful debugging features.
 ///
-/// [run_engine()] is the default core function.
 ///
 /// # Examples
 ///
-/// Any function that takes an [Engine] as an argument and does not have a return type can
-/// be used as an engine core.  To create a custom engine core, you just implement it as
-/// a function:
+/// To use the default main loop, you don't need to do anything special.  The engine will 
+/// automatically select [DefaultMainLoop] unless a different one is selected either by you, or, 
+/// more commonly, by a [Plugin](crate::Plugin).
+/// 
+/// If you wish to override the engine's main loop behavior, you can simply implement this trait
+/// on a struct.
 ///
 /// ```
 /// # use wolf_engine::*;
 /// #
-/// pub strcut CustomMainLoop;
+/// pub struct CustomMainLoop;
 ///
 /// impl MainLoop for CustomMainLoop {
 ///     fn run(&mut self, mut engine: Engine) -> Engine {
