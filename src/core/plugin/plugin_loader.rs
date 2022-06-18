4e8871a42e318a0d2f8d067ef7c21e44e66d37a1
AlexiWolf <alexiwolf@pm.me>
2022-06-18 17:22:55 -0500

    Fixed broken test

--- src/core/plugin/plugin_loader.rs
+++ src/core/plugin/plugin_loader.rs
@@ -110,7 +110,7 @@ mod plugin_loader_tests {
         let loader_result = plugin_loader.load_all(EngineBuilder::new());
     
         assert!(loader_result.is_err());
-        assert_eq!(loader_result.unwrap_err(), "Failed to load Test Plugin: Test Error");
+        assert_eq!(loader_result.err().unwrap(), "Failed to load Test Plugin: Test Error");
     }
 
     #[test]
