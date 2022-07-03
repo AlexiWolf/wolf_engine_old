d8500a2b91214a9f767e9457805dbe0c622b9ac4
AlexiWolf <alexiwolf@pm.me>
2022-07-02 21:29:26 -0500

    Moved `PuffinPlugin` behind the feature flag

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,7 +1,11 @@
 //! Provides built-in [Plugin](crate::Plugin) implementations.
 
 mod core_plugin;
+
+#[cfg(feature = "profiling")]
 mod puffin_plugin;
 
 pub(crate) use core_plugin::*;
+
+#[cfg(feature = "profiling")]
 pub use puffin_plugin::*;
