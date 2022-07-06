5fabfbc8c1295358bfce12b08129665b4a8c9b3e
AlexiWolf <alexiwolf@pm.me>
2022-07-06 01:42:26 -0500

    Removed `event_context` use

--- src/contexts/mod.rs
+++ src/contexts/mod.rs
@@ -6,7 +6,6 @@ mod puffin_http_context;
 mod scheduler_context;
 
 pub use engine_context::*;
-pub use event_context::*;
 #[cfg(feature = "http_profiling")]
 pub use puffin_http_context::*;
 pub use scheduler_context::*;
