731df21b6eb30b0452b14a46f41889c9c44f0550
AlexiWolf <alexiwolf@pm.me>
2022-08-29 15:18:32 -0500

    Removed all profiling features

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -2,10 +2,5 @@
 
 mod core_plugin;
 
-#[cfg(feature = "profiling")]
-mod puffin_plugin;
-
 pub(crate) use core_plugin::*;
 
-#[cfg(feature = "profiling")]
-pub use puffin_plugin::*;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,5 @@
 //! Provides common helpers and utilities.
 
 mod engine_controls;
-mod profiling;
 
 pub use engine_controls::*;
-pub use profiling::*;
