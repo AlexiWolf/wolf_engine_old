ca841b9bbc82360a6a4378f540b8e878eba9afdf
AlexiWolf <alexiwolf@pm.me>
2022-09-07 09:38:31 -0500

    Updated scheduelr documentation

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,74 +1,74 @@
-//! Provides flexible update and frame timing controls for the [Engine](crate::Engine).
+//! Provides flexible timing controls for the [Engine].
+//!
+//! A scheduler is responsible for determining if and when the [Engine] should run various 
+//! operations.  This includes when to run [State] updates, when to render frames, and when various 
+//! engine stages should run.
+//!
+//! Wolf Engine has two kinds of scheduler, [UpdateScheduler] to control game logic, and 
+//! [RenderScheduler] to control the rendering.  Refer to the respective trait's documentation
+//! for specific details.
+//!
+//! The scheduler traits make it easy to change the behavior of the [Engine] to suit your game's 
+//! needs. Different implementations may provide different behavior and / or additional features, 
+//! so you should refer to implementation docs for specific details.
+//!
+//! By default, the [Engine] will use a [FixedUpdateScheduler](crate::schedulers::FixedUpdateScheduler)
+//! and a [SimpleRenderScheduler](crate::schedulers::SimpleRenderScheduler).
+//!
+//! # Examples
+//!
+//! Alternative schedulers can be provided to the [Engine] at startup using 
+//! [EngineBuilder::with_update_scheduler()] and [EngineBuilder::with_render_scheduler()].
+//!
+//! ```
+//! # use wolf_engine::*;
+//! # use wolf_engine::schedulers::*;
+//! # 
+//! # let update_scheduler = FixedUpdateScheduler::default();
+//! # let render_scheduler = SimpleRenderScheduler;
+//! #
+//! let engine = EngineBuilder::new()
+//!     .with_update_scheduler(Box::from(update_scheduler))
+//!     .with_render_scheduler(Box::from(render_scheduler))
+//!     .build();
+//! ```
+//!
+//! Custom schedulers can be created by implementing a scheduler trait.
+//!
+//! ```
+//! # use wolf_engine::*;
+//! #
+//! pub struct MySimpleUpdateScheduler;
+//!
+//! impl UpdateScheduler for MySimpleUpdateScheduler {
+//!
+//!     fn update(&mut self, context: &mut Context, state: &mut dyn State) {
+//!         state.update(context);
+//!     }
+//! }
+//!
+//! pub struct MySimpleRenderScheduler;
+//!
+//! impl RenderScheduler for MySimpleRenderScheduler {
+//!     fn render(&mut self, context: &mut Context, state: &mut dyn State) {
+//!         state.render(context);
+//!     }
+//! }
+//! ```
 
-use crate::{Context, State};
+use crate::*;
 
 #[cfg(test)]
 use mockall::automock;
 
-/// Controls how the game is run.
-///
-/// The scheduler is responsible for determining if or when the game should be updated
-/// or rendered.  Schedulers are useful for creating predictable update timing, preventing
-/// busy loops, and implementing frame limiters, among other things.
-///
-/// Wolf Engine's default scheduler is the
-/// [FixedUpdateScheduler](crate::schedulers::FixedUpdateScheduler).
-///
-/// Different schedulers may operate differently, so you should refer to implementation
-/// documentation for specific details.
-///
-/// ## Updating
-///
-/// At it's most basic, the `update` function just calls the game's update function and
-/// returns the result from it.  Depending on the implementation, different update
-/// strategies may be used and / or additional timing controls may be added.
-///
-/// Ticks vs Updates:
-///
-/// - An *update* refers to a single call to the scheduler's `update` method.
-/// - A *tick* refers to a single step of the game's state.
-/// - There may be 0, 1, or any other number of *ticks* in an *update*.
-///
-/// # Creating a Custom Scheduler
-///
-/// Wolf Engine fully supports using a custom scheduler.  Simply implement this trait.
-///
-/// ```
-/// # use wolf_engine::*;
-/// #
-/// pub struct MyUpdateScheduler;
-///
-/// impl UpdateScheduler for MyUpdateScheduler {
-///
-///     fn update(&mut self, context: &mut Context, state: &mut dyn State) {
-///         // Add timing control logic here.
-///         state.update(context);
-///     }
-/// }
-/// ```
-///
-/// You can use a custom [Scheduler](crate::Scheduler) implementation by using
-/// the `EngineBuilder::with_scheduler()` method.
-///
-/// ```
-/// # use wolf_engine::*;
-/// # use wolf_engine::schedulers::FixedUpdateSchedulerBuilder;
-/// #
-/// # // Using fixed game loop for the example because the actual loop is unimportant.
-/// # // Any Scheduler can be provided here and it will work just the same.
-/// # let custom_scheduler = FixedUpdateSchedulerBuilder::new()
-/// #   .build();
-/// #
-/// let engine = EngineBuilder::new()
-///     .with_update_scheduler(Box::from(custom_scheduler))
-///     .build();
-/// ```
+/// Controls how and when the the game / engine state is updated.
 #[cfg_attr(test, automock)]
 pub trait UpdateScheduler {
     /// Update the game state.
     fn update(&mut self, context: &mut Context, state: &mut dyn State);
 }
 
+///
 #[cfg_attr(test, automock)]
 pub trait RenderScheduler {
     /// Render the current frame.
