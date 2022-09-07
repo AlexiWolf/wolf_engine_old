02ffb54897e785f7cb10c27d8ec192f15898e627
AlexiWolf <alexiwolf@pm.me>
2022-09-07 08:01:06 -0500

    Added test of `SimpleRenderScheduler`

--- src/schedulers/simple_render_scheduler.rs
+++ src/schedulers/simple_render_scheduler.rs
@@ -5,3 +5,20 @@ pub struct SimpleRenderScheduler;
 impl RenderScheduler for SimpleRenderScheduler {
     fn render(&mut self, context: &mut Context, state: &mut dyn State) {}
 }
+
+#[cfg(test)]
+mod simple_render_scheduler_tests {
+    use super::*;
+   
+    #[test]
+    fn should_run_state_render_once() {
+        let mut scheduler = SimpleRenderScheduler;
+        let mut context = Context::new();
+        let mut state = MockState::new();
+        state.expect_render()
+            .once()
+            .return_const(());
+
+        scheduler.render(&mut context, &mut state);
+    }
+}
