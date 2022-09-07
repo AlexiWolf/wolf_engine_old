353ac7247a4c77be06ec22a80d400d5c48582f25
AlexiWolf <alexiwolf@pm.me>
2022-09-07 08:09:32 -0500

    Renamed test to be more descriptive

--- src/schedulers/simple_render_scheduler.rs
+++ src/schedulers/simple_render_scheduler.rs
@@ -13,7 +13,7 @@ mod simple_render_scheduler_tests {
     use super::*;
    
     #[test]
-    fn should_run_state_render_once() {
+    fn should_run_state_render_once_per_call() {
         let mut scheduler = SimpleRenderScheduler;
         let mut context = Context::new();
         let mut state = MockState::new();
