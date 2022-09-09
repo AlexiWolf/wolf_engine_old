540f28904f655aebf0bd3cde501f9a9884592e43
AlexiWolf <alexiwolf@pm.me>
2022-09-09 01:13:21 -0500

    Condensed paragraphs into one, and adjusted wording

--- src/schedulers/mod.rs
+++ src/schedulers/mod.rs
@@ -2,11 +2,9 @@
 //!
 //! A scheduler is responsible for determining if and when the [Engine] should run various 
 //! operations.  This includes when to run [State] updates, when to render frames, and when to 
-//! run various engine stages.
-//!
-//! Wolf Engine has two kinds of scheduler, [UpdateScheduler] to control game logic, and 
-//! [RenderScheduler] to control the rendering.  Refer to the respective trait's documentation
-//! for specific details.
+//! run various engine stages.  Wolf Engine has two kinds of scheduler, [UpdateScheduler] to control
+//! game logic, and [RenderScheduler] to control rendering.  Refer to the respective trait's 
+//! documentation for specific details.
 //!
 //! The scheduler traits make it easy to change the behavior of the [Engine] to suit your game's 
 //! needs. Different implementations may provide different behavior and / or additional features, 
