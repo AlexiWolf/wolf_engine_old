eebb2b78297ded2d8b9a19eb0a8a218485b90e32
AlexiWolf <alexiwolf@pm.me>
2022-09-17 08:20:17 -0500

    Added test of stage callbacks

--- src/stage.rs
+++ src/stage.rs
@@ -14,6 +14,13 @@ mod stage_tests {
     #[test]
     fn should_create_empty_stage_callbacks() {
         let stage_callbacks = StageCallbacks::new();
+
+        assert_eq!(stage_callbacks.pre_update_callbacks.len(), 0);
+        assert_eq!(stage_callbacks.update_callbacks.len(), 0);
+        assert_eq!(stage_callbacks.post_update_callbacks.len(), 0);
+        assert_eq!(stage_callbacks.pre_render_callbacks.len(), 0);
+        assert_eq!(stage_callbacks.render_callbacks.len(), 0);
+        assert_eq!(stage_callbacks.post_render_callbacks.len(), 0);
     }
 
     #[test]
