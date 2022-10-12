5de775f9214e86897a1f544fd2a76d45c29bf420
AlexiWolf <alexiwolf@pm.me>
2022-10-11 22:21:53 -0500

    Added `wolf_engine::prelude`

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,9 @@ pub use wolf_engine_framework as framework;
 
 #[cfg(feature = "logging")]
 pub use wolf_engine_framework::logging as logging;
+
+pub mod prelude {
+    pub use super::*;
+    #[cfg(feature = "framework")]
+    pub use framework::*;
+}
