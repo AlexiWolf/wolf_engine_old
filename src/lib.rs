ff3295f210fc2bd9452bf00bb808ebc8fc1c7252
AlexiWolf <alexiwolf@pm.me>
2022-10-11 19:11:17 -0500

    Added reexport for logging module

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@ pub use wolf_engine_core::*;
 
 #[cfg(feature = "framework")]
 pub use wolf_engine_framework as framework;
+
+#[cfg(feature = "logging")]
+pub use wolf_engine_framework::logging as logging;
