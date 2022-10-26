c92e504da3001450b92200947a05c7c779de5a07
AlexiWolf <alexiwolf@pm.me>
2022-10-26 12:48:59 -0500

    Removed all code from `framework`
    I'm unhappy with the way the engine comes together currently.  It could be done
    better, and that's what this feature is going to address.  For now, I'm
    stripping out all the existing code and starting fresh.  Components will be
    readded, and updated to work with the new engine later.

--- wolf_engine_framework/src/contexts/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Provides built-in [Subcontext](wolf_engine_core::Subcontext) implementations.
-
-mod engine_context;
-mod scheduler_context;
-
-pub use engine_context::*;
-pub use scheduler_context::*;
--- wolf_engine_framework/src/plugins/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Provides built-in [Plugin](crate::Plugin) implementations.
-
-mod core_plugin;
-
-pub(crate) use core_plugin::*;
