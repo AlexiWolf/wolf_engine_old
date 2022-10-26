3351a9b3e58a8d723f053011e76a983c9433d219
AlexiWolf <alexiwolf@pm.me>
2022-10-26 14:16:28 -0500

    Added `Context` trait

--- wolf_engine_core/src/engine.rs
+++ wolf_engine_core/src/engine.rs
@@ -1,11 +1,13 @@
+pub trait Context {}
 
 #[cfg(test)]
 mod engine_tests {
+    use super::*;
+
     struct TestData {}
 
     impl TestData { 
         pub fn new() -> Self { Self }
-
     }
 
     impl Context for TestData {}
