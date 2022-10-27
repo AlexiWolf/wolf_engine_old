16885d458f324d7aeec40832d021af6dbfb7d1bb
AlexiWolf <alexiwolf@pm.me>
2022-10-27 03:10:29 -0500

    Added test of empty event queue

--- wolf_engine_core/src/engine.rs
+++ wolf_engine_core/src/engine.rs
@@ -92,5 +92,7 @@ mod engine_tests {
     #[test]
     fn should_emit_events_cleared_when_event_queue_is_empty() {
         let mut engine = Engine::new(TestData::new()); 
+
+        assert_eq!(engine.next_event().unwrap(), Event::EventsCleared);
     }
 }
