f9deff16c7fe71a5e1cf21722be6d1390de92aba
AlexiWolf <alexiwolf@pm.me>
2022-10-28 15:37:05 -0500

    Added documentation for `EventLoop`

--- wolf_engine_core/src/events/event_loop.rs
+++ wolf_engine_core/src/events/event_loop.rs
@@ -1,4 +1,8 @@
+/// Provides the main event-loop functions used by the [Engine].
 pub trait EventLoop<E> {
+    /// Returns the next event in the loop.
     fn next_event(&self) -> Option<E>;
+
+    /// Send an event into the loop.
     fn send_event(&self, event: E);
 }
