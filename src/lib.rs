af435449595af183c7fa5039b15ff6fa0036612d
AlexiWolf <alexiwolf@pm.me>
2022-10-29 15:12:05 -0500

    Hid the prelude from showing up in the docs

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,8 @@ pub use wolf_engine_framework as framework;
 #[cfg(feature = "logging")]
 pub use wolf_engine_core::logging;
 
+#[doc(hidden)]
 pub mod prelude {
-    //! Provides the prelude for Wolf Engine.
     pub use super::*;
     #[cfg(feature = "framework")]
     pub use framework::*;
