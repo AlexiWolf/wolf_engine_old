0bd3c9064d75a4b2ef1684ef3146aca5fe51bd4c
AlexiWolf <alexiwolf@pm.me>
2022-10-29 13:53:38 -0500

    Fixed broken links in `Context` docs

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -1,13 +1,14 @@
 use crate::events::EventLoop;
 
-/// A marker trait indicating which types can be used as context-data on the [`Engine`].
+/// A marker trait indicating which types can be used as context-data on the
+/// [`Engine`](crate::Engine).
 ///
 /// Thit trait is automatically implemented for all types meeting the type constraints, so you
 /// don't normally need to implement it yourself.
 ///
 /// To be used as a context, a type needs to:
 ///
-/// - Implement [`Eventloop`].
+/// - Implement [`EventLoop`].
 pub trait Context<E>: EventLoop<E> {}
 
 impl<T, E> Context<E> for T where T: EventLoop<E> {}
