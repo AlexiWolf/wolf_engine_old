a865a3bc4c081763adcc3daf7f677c4e70ab98eb
AlexiWolf <alexiwolf@pm.me>
2022-10-31 17:11:11 -0500

    Implemented the example

--- examples/core_engine_basics.rs
+++ examples/core_engine_basics.rs
@@ -2,13 +2,23 @@ use wolf_engine::prelude::*;
 
 pub fn main() {
     let mut engine = Engine::default();
+    let mut number = 0;
 
     while let Some(event) = engine.next_event() {
         match event {
-            Event::Quit => todo!(),
-            Event::Update => todo!(),
-            Event::Render => todo!(),
-            Event::EventsCleared => todo!(),
+            Event::Quit => println!("Quit event recieved.  Goodbye!"),
+            Event::Update => if number == 3 {
+                engine.send_event(Event::Quit); 
+            } else {
+                number += 1;
+            },
+            Event::Render => {
+                println!("{}: Hello, World!", number);
+            }
+            Event::EventsCleared => {
+                engine.send_event(Event::Update);
+                engine.send_event(Event::Render);
+            }
         }
     }
 }
