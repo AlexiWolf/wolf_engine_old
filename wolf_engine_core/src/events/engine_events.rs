2544a912b5bf814a8bf05bb9b7349d2f63fc987e
AlexiWolf <alexiwolf@pm.me>
2022-10-30 20:28:17 -0500

    Added docs to `Event`

--- wolf_engine_core/src/events/engine_events.rs
+++ wolf_engine_core/src/events/engine_events.rs
@@ -1,7 +1,14 @@
+/// Provides the main events used by the [`Engine`].
 #[derive(Debug, PartialEq, Eq)]
 pub enum Event {
+    /// Emitted when the engine should quit.
     Quit,
+    /// Indicates the engine should update game logic.
     Update,
+    /// Indicates the engine should render the game.
     Render,
+    /// Indicates the end of a frame.
+    ///
+    /// `EventsCleared` should be emitted only after all other events have been processed.
     EventsCleared,
 }
