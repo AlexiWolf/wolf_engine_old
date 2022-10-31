4fe4211d7fcdc00eb2d94c660aa5a16690b492cf
AlexiWolf <alexiwolf@pm.me>
2022-10-31 16:03:31 -0500

    Added `match` against `Event` to example

--- examples/core_engine_basics.rs
+++ examples/core_engine_basics.rs
@@ -4,6 +4,11 @@ pub fn main() {
     let mut engine = Engine::default();
 
     while let Some(event) = engine.next_event() {
-
+        match event {
+            Event::Quit => todo!(),
+            Event::Update => todo!(),
+            Event::Render => todo!(),
+            Event::EventsCleared => todo!(),
+        }
     }
 }
