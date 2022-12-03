130196c80f5993439a049a011d40b0469deddd82
AlexiWolf <alexiwolf@pm.me>
2022-12-02 22:18:17 -0600

    Implemented `Default` for `WindowSettings`

--- wolf_engine_window/src/lib.rs
+++ wolf_engine_window/src/lib.rs
@@ -1,9 +1,21 @@
-pub struct WindowSettings {}
+pub struct WindowSettings {
+    pub title: String,
+    pub width: usize,
+    pub height: usize,
+    pub is_fullscreen: bool,
+    pub is_borderless: bool,
+    pub is_resizable: bool,
+}
 
 impl Default for WindowSettings {
     fn default() -> Self {
         Self {
-
+            title: "Wolf Engine - Untitled Window".to_string(),
+            width: 1280,
+            height: 720,
+            is_fullscreen: false,
+            is_borderless: false,
+            is_resizable: true,
         }
     }
 }
