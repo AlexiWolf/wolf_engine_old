a08a99d4093af5ed1de056383b98f0f4e03657ca
AlexiWolf <alexiwolf@pm.me>
2022-12-02 22:16:13 -0600

    Added expected fields, and default values to test

--- wolf_engine_window/src/lib.rs
+++ wolf_engine_window/src/lib.rs
@@ -15,5 +15,16 @@ mod window_settings_tests {
     #[test]
     fn should_implement_default_trait() {
         let window_settings = WindowSettings::default();
+        assert_eq!(
+            window_settings,
+            WindowSettings {
+                title: "Wolf Engine - Untitled Window",
+                width: 1280,
+                height: 720,
+                is_fullscreen: false,
+                is_borderless: false,
+                is_resizable: true,
+            }
+        );
     }
 }
