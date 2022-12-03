9a575f164631271b828b2b7501f226ef065681c6
AlexiWolf <alexiwolf@pm.me>
2022-12-02 22:11:28 -0600

    Added stub of `WindowSettings`

--- wolf_engine_window/src/lib.rs
+++ wolf_engine_window/src/lib.rs
@@ -1,5 +1,9 @@
+pub struct WindowSettings {}
+
 #[cfg(test)]
 mod window_settings_tests {
+    pub use super::*;
+
     #[test]
     fn should_implement_default_trait() {
         let window_settings = WindowSettings::default();
