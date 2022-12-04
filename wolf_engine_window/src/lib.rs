5111d5c5cc7d425a1ddab22021a64c92d68f58b2
AlexiWolf <alexiwolf@pm.me>
2022-12-04 15:27:17 -0600

    Removed usage example

--- wolf_engine_window/src/lib.rs
+++ wolf_engine_window/src/lib.rs
@@ -1,19 +1,5 @@
 //! Provides a high-level, back-end agnostic window API for [Wolf
 //! Engine](https://docs.rs/wolf_engine/latest).
-//!
-//! # Examples
-//!
-//! ```
-//! # use wolf_engine_window::prelude::*;
-//! # 
-//! # let mut window_backend = TestWindowBackend;
-//! #
-//! let window_settings = WindowSettings::new()
-//!     .with_title("Example Window")
-//!     .with_size((800, 600));
-//! let window = window_backend.create_window(window_settings)
-//!     .unwrap();
-//! ```
 
 mod window_settings;
 pub use window_settings::*;
