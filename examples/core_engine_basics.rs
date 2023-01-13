2604580768ccdafc20b7e769cf5d6b9f0b5b6506
AlexiWolf <alexiwolf@pm.me>
2023-01-13 15:02:54 -0600

    Added new API / usage for `Context`

--- examples/core_engine_basics.rs
+++ examples/core_engine_basics.rs
@@ -1,33 +1,35 @@
 use wolf_engine::prelude::*;
 
+pub struct GameData {
+    pub number: i32,
+}
+
 pub fn main() {
-    let mut engine = Engine::default();
-    let mut number = 0;
+    let mut engine = Engine::from(GameData { number: 0 });
 
     while let Some(event) = engine.next_event() {
-        match event {
-            // Shut down the game.
-            Event::Quit => println!("Quit event received.  Goodbye!"),
-            // Update the game.
-            Event::Update => {
-                if number == 3 {
-                    // To shut down the Engine, you must send a quit event.
-                    engine.quit();
-                } else {
-                    number += 1;
-                }
+        process_event(event, engine.context_mut());        
+    }
+}
+
+pub fn process_event(event: Event, context: &mut Context) {
+    match event {
+        // Shut down the game.
+        Event::Quit => println!("Quit event received.  Goodbye!"),
+        // Update the game.
+        Event::Update => {
+            if context.data.number == 3 {
+                // To shut down the Engine, you must send a quit event.
+                context.quit();
+            } else {
+                context.data.number += 1;
             }
-            Event::Render => {
-                // Render the game.
-                println!("{}: Hello, World!", number);
-            }
-            Event::EventsCleared => {
-                // Note: The engine will not emit Update / Render events on it's own.
-                //       You are expected to do this yourself.
-                engine.update();
-                engine.render();
-            }
-            _ => (),
         }
+        Event::EventsCleared => {
+            // Note: The engine will not emit Update / Render events on it's own.
+            //       You are expected to do this yourself.
+            context.update();
+        }
+        _ => (),
     }
 }
