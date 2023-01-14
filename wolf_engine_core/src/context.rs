50b81cd473da90649b951d538d424ba20044b958
AlexiWolf <alexiwolf@pm.me>
2023-01-13 18:39:01 -0600

    Added stub of `EngineControls` impl for `Context`

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -1,3 +1,5 @@
+use crate::EngineControls;
+
 pub struct Context<D> {
     pub data: D,
 }
@@ -5,5 +7,20 @@ pub struct Context<D> {
 impl<D> From<D> for Context<D> {
     fn from(data: D) -> Self {
         Self { data }
+    }
+}
+
+impl<D> EngineControls for Context<D> {
+    fn quit(&self) {
+    }
+
+    fn has_quit(&self) -> bool {
+        false
+    }
+
+    fn update(&self) {
+    }
+
+    fn render(&self) {
     }
 }
