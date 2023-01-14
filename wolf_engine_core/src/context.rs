cdbbf0d344c18e23e485c973329a96212e4d35a8
AlexiWolf <alexiwolf@pm.me>
2023-01-13 22:14:35 -0600

    Added `event_sender` to `Context`

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -1,17 +1,28 @@
-use crate::EngineControls;
+use std::sync::mpsc::Sender;
 
-pub struct Context<D> {
+use crate::{EngineControls, prelude::Event};
+
+pub struct Context<'a, D> {
     pub data: D,
+    event_sender: &'a Sender<Event>,
     has_quit: bool,
 }
 
-impl<D> Context<D> {
+impl<'a, D> Context<'a, D> {
+    pub fn new(event_sender: &Sender<Event>, data: D) -> Self {
+        Self {
+            data,
+            event_sender,
+            has_quit: false,
+        }
+    }
+
     pub(crate) fn set_has_quit(&mut self, has_quit: bool) {
         self.has_quit = has_quit;
     }
 }
 
-impl<D> EngineControls for Context<D> {
+impl<'a, D> EngineControls for Context<'a, D> {
     fn quit(&self) {
         
     }
