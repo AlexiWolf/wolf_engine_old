c519e19f38662d9e332ba5bc2ab15289eadab64e
AlexiWolf <alexiwolf@pm.me>
2023-01-13 22:11:19 -0600

    Removed `From<D>` impl for `Context`

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -3,21 +3,11 @@ use crate::EngineControls;
 pub struct Context<D> {
     pub data: D,
     has_quit: bool,
-    
 }
 
 impl<D> Context<D> {
     pub(crate) fn set_has_quit(&mut self, has_quit: bool) {
         self.has_quit = has_quit;
-    }
-}
-
-impl<D> From<D> for Context<D> {
-    fn from(data: D) -> Self {
-        Self { 
-            data,
-            has_quit: false,
-        }
     }
 }
 
