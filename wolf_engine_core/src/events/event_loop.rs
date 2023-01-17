1944e0476e462d1ca42da9d8f8133b1448d4a844
AlexiWolf <alexiwolf@pm.me>
2023-01-16 19:05:07 -0600

    Added `EventLoop::sender`, and `EventSender` triat

--- wolf_engine_core/src/events/event_loop.rs
+++ wolf_engine_core/src/events/event_loop.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 /// Provides the main event-loop functions used by the [`Engine`](crate::Engine).
 pub trait EventLoop<E> {
     /// Returns the next event in the loop.
@@ -5,4 +7,10 @@ pub trait EventLoop<E> {
 
     /// Send an event into the loop.
     fn send_event(&self, event: E);
+
+    fn sender(&self) -> Arc<dyn EventSender<E>>;
+}
+
+pub trait EventSender<E> {
+    fn send(&self, event: E);
 }
