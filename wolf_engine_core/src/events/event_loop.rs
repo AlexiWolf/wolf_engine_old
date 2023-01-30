1e964cfae60f37c56312180109f2747ebf36c61d
AlexiWolf <alexiwolf@pm.me>
2023-01-30 01:15:11 -0600

    Removed original `EventSender`

--- wolf_engine_core/src/events/event_loop.rs
+++ wolf_engine_core/src/events/event_loop.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use super::EventSenderProxy;
+
 /// Provides the main event-loop functions used by the [`Engine`](crate::Engine).
 pub trait EventLoop<E>: HasEventSenderProxy<E> {
     /// Returns the next event in the loop.
@@ -10,19 +12,4 @@ pub trait EventLoop<E>: HasEventSenderProxy<E> {
 pub trait HasEventSenderProxy<E> {
     /// Create a new [`EventSender`] from this `EventLoop`.
     fn sender(&self) -> Arc<dyn EventSenderProxy<E>>;
-}
-
-/// A thread-safe proxy for sending events to the associated [`EventLoop`].
-///
-/// There may be many copies of an `EventSender`, and the [`EventLoop`] may have any number of
-/// event senders associated with it.  An `EventSender` is valid as long as the connected
-/// [`EventLoop`] hasn't been dropped.  The `EventSender` can be cloned, and passed around freely,
-/// even across thread boundaries.
-pub trait EventSenderProxy<E>: Send + Sync {
-    /// Send an event to the associated [`EventLoop`].
-    ///
-    /// This method will return [`Ok`] if the event is sent successfully.  An [`Err`] normally
-    /// means the [`EventLoop`] has been dropped, or is no longer reachable for any reason.  The
-    /// error message should provide more details.
-    fn send_event(&self, event: E) -> Result<(), String>;
 }
