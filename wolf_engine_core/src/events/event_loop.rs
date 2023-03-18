c100bf9290272a5994c70ef77b4804ee93e5a4f2
AlexiWolf <alexiwolf@pm.me>
2023-03-18 12:48:41 -0500

    Updated documentation for event types

--- wolf_engine_core/src/events/event_loop.rs
+++ wolf_engine_core/src/events/event_loop.rs
@@ -2,14 +2,18 @@ use std::sync::Arc;
 
 use super::EventSenderProxy;
 
-/// Provides the main event-loop functions used by the [`Engine`](crate::Engine).
+/// A generic, FIFO event queue.
+///
+/// The `EventQueue` uses a split receiver / sender design, similar to [`std::sync::mpsc::channel`] 
+/// where events are sent in by an associated [`EventSender`](super::EventSender) or 
+/// [`EventSenderProxy`].  The order of incoming events is always preserved.
 pub trait EventLoop<E>: HasEventSenderProxy<E> {
-    /// Returns the next event in the loop.
+    /// Returns the next event in the queue.
     fn next_event(&mut self) -> Option<E>;
 }
 
-/// A type from which an [`EventSender`](super::EventSender) can be created.
+/// A type from which an [`EventSenderProxy`](super::EventSenderProxy) can be created.
 pub trait HasEventSenderProxy<E> {
-    /// Create a new [`EventSender`](super::EventSender) from this `EventLoop`.
+    /// Create a new [`EventSenderProxy`](super::EventSenderProxy).
     fn event_sender(&self) -> Arc<dyn EventSenderProxy<E>>;
 }
--- wolf_engine_core/src/events/event_sender.rs
+++ wolf_engine_core/src/events/event_sender.rs
@@ -1,5 +1,11 @@
+/// A type which can send events.  Most commonly used to send events back to an 
+/// [`EventLoop`](super::EventLoop).
+///
+/// In cases where you need to send events across threads, or you need to skirt around borrowing
+/// rules, using an [`EventSenderProxy`] may be needed.
 pub trait EventSender<E> {
     fn send_event(&self, event: E) -> Result<(), String>;
 }
 
+/// A thread-safe link to an [`EventSender`].
 pub trait EventSenderProxy<E>: EventSender<E> + Send + Sync {}
