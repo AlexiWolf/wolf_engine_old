870770675112fcd57fb1485464ab1117330bf7a9
AlexiWolf <alexiwolf@pm.me>
2023-03-19 12:28:13 -0500

    Added usage examples to `EventQueue`

--- wolf_engine_core/src/events/event_queue.rs
+++ wolf_engine_core/src/events/event_queue.rs
@@ -5,6 +5,48 @@ use std::sync::Arc;
 /// The `EventQueue` uses a split receiver / sender design, similar to [`std::sync::mpsc::channel`] 
 /// where events are sent in by an associated [`EventSender`].  The order of incoming events is 
 /// always preserved.
+///
+/// # Examples
+/// 
+/// Iterating over all events using a `while let` loop.
+///
+/// ```
+/// # use wolf_engine_core::events::*;
+/// #
+/// # let mut event_queue = MpscEventQueue::<Event>::new();
+/// while let Some(event) = event_queue.next_event() {
+///     // Do something cool.
+/// }
+/// ```
+/// 
+/// Sending an event through an [`EventSender`].
+///
+/// ```
+/// # use wolf_engine_core::events::*;
+/// #
+/// # enum EventType { Event }
+/// #
+/// # let event_queue = MpscEventQueue::<EventType>::new();
+/// let event_sender = event_queue.event_sender();
+/// event_sender.send_event(EventType::Event);
+/// ```
+///
+/// Sending an event across threads.
+///
+/// ```
+/// # use wolf_engine_core::events::*;
+/// #
+/// # enum EventType { Event };
+/// #
+/// # let event_queue = MpscEventQueue::new();
+/// let event_sender = event_queue.event_sender();
+///
+/// std::thread::spawn(move || {
+///     event_sender.send_event(EventType::Event).unwrap();
+/// })
+/// # .join()
+/// # .unwrap();
+/// ```
 pub trait EventQueue<E>: HasEventSender<E> {
     /// Returns the next event in the queue.
     fn next_event(&mut self) -> Option<E>;
