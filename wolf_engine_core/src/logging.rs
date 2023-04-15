fed9c321f5f6c78bbf9b57a25cb0f55177c59756
AlexiWolf <alexiwolf@pm.me>
2023-04-14 21:45:51 -0500

    Added `Into<LevelFilter>` impl for `LogLevel` enum

--- wolf_engine_core/src/logging.rs
+++ wolf_engine_core/src/logging.rs
@@ -6,6 +6,14 @@ pub enum LogLevel {
     Debug,
 }
 
+impl Into<log::LevelFilter> for LogLevel {
+    fn into(self) -> log::LevelFilter {
+        match self {
+            Self::Debug => log::LevelFilter::Debug,
+        }
+    }
+}
+
 /// Initializes the logging system with a pre-configured [SimpleLogger] instance.
 ///
 /// This function is provided for those who don't need a complicated logging setup.  Messages will
@@ -31,7 +39,7 @@ pub enum LogLevel {
 pub fn initialize_logging(log_level: LogLevel) {
     SimpleLogger::new()
         .with_colors(true)
-        .with_level(log_level)
+        .with_level(log_level.into())
         .with_utc_timestamps()
         .init()
         .expect("Failed to initialize the logger");
