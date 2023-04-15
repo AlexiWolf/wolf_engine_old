ef441a388794b5241965400cc54710e6eb424f72
AlexiWolf <alexiwolf@pm.me>
2023-04-14 21:57:08 -0500

    Updated multi-threadded logging test

--- tests/multi_thread_logging.rs
+++ tests/multi_thread_logging.rs
@@ -4,10 +4,11 @@ pub mod multi_threaded_logging_tests {
     use log::*;
     use std::thread;
     use wolf_engine::*;
+    use wolf_engine::logging::*;
 
     #[test]
     pub fn should_not_panic_in_multi_threaded_environment() {
-        logging::initialize_logging(LevelFilter::Info);
+        logging::initialize_logging(LogLevel::Info);
 
         let thread = thread::spawn(|| {
             info!("Hello, world!");
