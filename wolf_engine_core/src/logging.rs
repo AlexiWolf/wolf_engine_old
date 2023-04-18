1009e1542f005e0d0cdc2de79a9f35d56c0098b2
AlexiWolf <alexiwolf@pm.me>
2023-04-18 05:27:34 -0500

    Added docs to the `LogLevel` enum

--- wolf_engine_core/src/logging.rs
+++ wolf_engine_core/src/logging.rs
@@ -4,11 +4,23 @@ use simple_logger::SimpleLogger;
 
 /// Indicates the verbosity of the log system.
 pub enum LogLevel {
+
+    /// Log all messages.
     Trace,
+
+    /// Log debug messages.
     Debug,
+    
+    /// Log info messages.
     Info,
+    
+    /// Log warning messages.
     Warn,
+
+    /// Log error messages.
     Error,
+    
+    /// Disable log messages.
     Off,
 }
 
