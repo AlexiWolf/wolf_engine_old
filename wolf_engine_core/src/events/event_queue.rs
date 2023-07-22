497685fbe889fcbfb469280cafd1e2cb80b2a552
AlexiWolf <alexi@wolvesin.space>
2023-07-22 14:35:14 -0500

    Updated docs for `events` module traits

--- wolf_engine_core/src/events/event_queue.rs
+++ wolf_engine_core/src/events/event_queue.rs
@@ -10,12 +10,20 @@ pub trait EventQueue<E>: HasEventSender<E> {
 }
 
 /// A type which has an [`EventSender`].
+///
+/// A complete guide, with examples, on how use trait can be found in the main 
+/// [`events module`](crate::events) documentation.
 pub trait HasEventSender<E> {
     /// Creates a new [`EventSender`].
     fn event_sender(&self) -> Arc<dyn EventSender<E>>;
 }
 
-/// A thread-safe link to an `EventQueue` for sending events.
+/// The event-sending half of an [`EventQueue`]. 
+///
+/// Event Senders can be freely, and safely cloned, given away, and even sent across threads.
+///
+/// A complete guide, with examples, on how use trait can be found in the main 
+/// [`events module`](crate::events) documentation.
 pub trait EventSender<E>: Send + Sync {
     fn send_event(&self, event: E) -> Result<(), String>;
 }
