9fbfd7495a75a51c0a614fd9b0d1b98f24d04a40
AlexiWolf <alexi@wolvesin.space>
2023-07-25 02:41:44 -0500

    Added docs, and basic usage example to `init()`

--- wolf_engine_core/src/lib.rs
+++ wolf_engine_core/src/lib.rs
@@ -25,6 +25,28 @@ pub mod prelude {
     pub use events::*;
 }
 
+/// Initializes a new instance of the [`EventLoop`], and its associated [`Context`], with the
+/// provided data.
+///
+/// #  Examples
+///
+/// ```
+/// # use wolf_engine_core as wolf_engine;
+/// # pub struct SomeData {};
+/// # let some_data = SomeData {};
+/// #
+/// // The prelude brings in commonly needed types, and traits.
+/// use wolf_engine::prelude::*;
+///
+/// // Start by initializing the EventLoop, and Context.
+/// let (mut event_loop, mut context) = wolf_engine::init(some_data);
+/// 
+/// // Then, you can use the EventLoop to run your game's main-loop.
+/// while let Some(event) = event_loop.next_event() {
+///     // Do something cool!
+/// #   break;
+/// }
+/// ```
 pub fn init<D>(data: D) -> (EventLoop, Context<D>) {
     let event_loop = EventLoop::new();
     let context = Context::new(&event_loop, data);
