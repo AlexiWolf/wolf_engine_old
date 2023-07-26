1932041afcec9934dbd0b96f1c2dc3abe47e5225
AlexiWolf <alexi@wolvesin.space>
2023-07-25 22:42:36 -0500

    Removed TODO

--- wolf_engine_core/src/lib.rs
+++ wolf_engine_core/src/lib.rs
@@ -1,13 +1,12 @@
 //! Provides common tools, types, and functions for the engine.
 //!
-//! The Core API provides all the parts likely to be (re)used by other parts of the event_loop.  It is
-//! mostly intended for those building, or making extensions to Wolf Engine, but there are some
+//! The Core API provides all the parts likely to be (re)used by other parts of the event_loop.  It
+//! is mostly intended for those building, or making extensions to Wolf Engine, but there are some
 //! tools for end-users as well.
 //!
 //! # Getting Started
 //!
-//! TODO: Flesh out the docs into a useful guide to the core API.  This will be done after
-//! currently-planned overhauls are made.
+//!
 
 mod context;
 pub use context::*;
