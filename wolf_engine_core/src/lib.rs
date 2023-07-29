690f19d047def6ebb3b9587d58c6f5fabf3a6b45
AlexiWolf <alexi@wolvesin.space>
2023-07-28 19:05:00 -0500

    Added basic usage example for the core module

--- wolf_engine_core/src/lib.rs
+++ wolf_engine_core/src/lib.rs
@@ -6,7 +6,46 @@
 //!
 //! # Getting Started
 //!
+//! ```
+//! # use wolf_engine_core as wolf_engine;
+//! use wolf_engine::prelude::*;
 //! 
+//! pub struct GameData {
+//!     pub number: i32,
+//! }
+//! 
+//! pub fn main() {
+//!     let (mut event_loop, mut context) = wolf_engine::init(GameData { number: 0 });
+//! 
+//!     while let Some(event) = event_loop.next_event() {
+//!         process_event(event, &mut context);
+//!     }
+//! }
+//! 
+//! pub fn process_event(event: Event, context: &mut Context<GameData>) {
+//!     match event {
+//!         // Shut down the game.
+//!         Event::Quit => println!("Quit event received.  Goodbye!"),
+//!         // Update the game.
+//!         Event::Update => {
+//!             if context.data.number == 3 {
+//!                 // To shut down the Engine, you must send a quit event.
+//!                 context.quit();
+//!             } else {
+//!                 context.data.number += 1;
+//!             }
+//!         }
+//!         Event::Render => println!("{}", context.data.number),
+//!         Event::EventsCleared => {
+//!             // Note: The engine will not emit Update / Render events on it's own.
+//!             //       You are expected to do this yourself.
+//!             context.update();
+//!             context.render();
+//!         }
+//!         _ => (),
+//!     }
+//! }
+//! ```
 
 mod context;
 pub use context::*;
