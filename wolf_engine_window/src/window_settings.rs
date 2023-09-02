c514c8f457ac1752d1b16d898cd98dd6b645543f
AlexiWolf <alexi@wolvesin.space>
2023-09-01 21:44:57 -0500

    Removed `FullscreenMode`

--- wolf_engine_window/src/window_settings.rs
+++ wolf_engine_window/src/window_settings.rs
@@ -1,18 +1,6 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use crate::WindowDimensions;
-
-/// Represents the fullscreen mode.
-#[derive(Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub enum FullscreenMode {
-    /// Exclusive fullscreen mode.
-    Fullscreen,
-    /// Borderless fullscreen mode.
-    Borderless,
-}
-
 /// Provides the settings used to create a window.
 #[derive(Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
