064f6b24f630f6de244c4019bf17b46fc16d6e9e
AlexiWolf <alexi@wolvesin.space>
2023-09-01 21:34:24 -0500

    Removed `Windowbackend`

--- wolf_engine_window/src/window.rs
+++ wolf_engine_window/src/window.rs
@@ -1,24 +1,2 @@
-use crate::*;
-
-use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
-
-#[cfg(test)]
-use mockall::{automock, mock};
-
-#[cfg(test)]
-use raw_window_handle::{RawDisplayHandle, RawWindowHandle};
-
-/// Provides a high-level API for creating, and working with [`Windows`](Window).
-#[cfg_attr(test, automock(type Window = MockWindow;))]
-pub trait WindowBackend {
-    /// The [`Window`] type used by this window implementation.
-    type Window: Window;
-
-    /// Create a window with the provided settings.
-    ///
-    /// Returns a [`Result`] containing a [`Window`], or a message explaining what went wrong.
-    fn create_window(&mut self, settings: WindowSettings) -> Result<Self::Window, String>;
-}
-
 /// Provides a high-level, back-end agnostic window API.
 pub trait Window {}
