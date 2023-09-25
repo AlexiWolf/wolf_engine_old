18f4861f1f6b92edf5f9ad09f09104f0aa1ef349
AlexiWolf <alexi@wolvesin.space>
2023-09-25 11:52:10 -0500

    Removed a bulk of the `Context` documentation
    It's no longer accurate.

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -10,47 +10,6 @@ use crate::events::*;
 ///
 /// The Context owns all engine data, sub-systems, and the link to the Event-Loop through which
 /// all events are sent.  As far as the end-user is concerned, the Context *is* the engine.
-///
-/// # Examples
-///
-/// ## Creating a `Context`
-///
-/// A good chunk of Wolf Engine's API depends on the Context.  As such, you will very likely need
-/// to create one *before* trying to use any of the rest of the API.  You can initialize a Context,
-/// along with its associated [`EventLoop`](crate::EventLoop), by calling [`wolf_engine::init()`](crate::init()).
-///
-/// ```
-/// # use wolf_engine_core as wolf_engine;
-/// let (mut event_loop, mut context) = wolf_engine::init::<()>();
-/// ```
-///
-/// ## Context Data
-///
-/// It's possible to extend the engine with custom functionality by using Context Data.  
-///
-/// Context data can be any type, and is provided to [`wolf_engine::init()`](crate::init()) at
-/// startup.  This data is mostly intended to be *engine data* such as sub-systems, however,
-/// there are no specific requirements around what the data is used for.  
-///
-/// If most cases, context data is only useful to those writing extensions for Wolf Engine, or for
-/// those who are building their own framework on top of Wolf Engine's Core API.  Users of
-/// the `framework` feature will use the Context data provided by the framework.
-///
-/// ```
-/// # use wolf_engine_core as wolf_engine;
-/// #
-/// // Create a type for your custom data.
-/// struct CustomContextData {
-///     // Custom sub-system data goes here.
-/// }
-/// #
-/// # impl CustomContextData {
-/// #   pub fn new() -> Self { Self {} }
-/// # }
-///
-/// // Initialize the engine with your custom data.
-/// let (mut event_loop, mut context) = wolf_engine::init::<()>(CustomContextData::new());
-/// ```
 pub struct Context<E: UserEvent> {
     event_sender: Arc<dyn EventSender<Event<E>>>,
 }
