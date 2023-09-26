52175f9ad4641400c511fe3c314f030171786aa1
AlexiWolf <alexi@wolvesin.space>
2023-09-26 15:17:02 -0500

    Removed "helper" fns, and their tests
    I'm thinking it may be better to just use the Legion types directly.  I also
    don't want to have to write delegates for everything.

--- wolf_engine_core/src/context.rs
+++ wolf_engine_core/src/context.rs
@@ -46,22 +46,6 @@ impl<E: UserEvent> Context<E> {
         &mut self.resources
     }
 
-    pub fn insert_resource<T: 'static>(&mut self, resource: T) {
-        self.resources.insert(resource);
-    }
-
-    pub fn remove_resource<T: 'static>(&mut self) -> Option<T> {
-        self.resources.remove::<T>()
-    }
-
-    pub fn resource<T: 'static>(&self) -> Option<AtomicRef<T>> {
-        self.resources.get::<T>()
-    }
-
-    pub fn resource_mut<T: 'static>(&self) -> Option<AtomicRefMut<T>> {
-        self.resources.get_mut()
-    }
-
     pub fn quit(&self) {
         self.event_sender.send_event(Event::Quit).ok();
     }
@@ -73,29 +57,21 @@ impl<E: UserEvent> HasEventSender<Event<E>> for Context<E> {
     }
 }
 
-
 #[cfg(test)]
-mod test_fixtures {
+mod context_tests {
     use super::*;
 
     use crate::EventLoop;
-
-    pub struct TestResource(pub &'static str);
 
     pub fn init() -> (EventLoop<()>, Context<()>) {
         let event_loop = EventLoop::<()>::new();
         let context = Context::new(&event_loop);
         (event_loop, context)
     }
-}
-
-#[cfg(test)]
-mod context_world_tests {
-    use super::*;
    
     #[test]
     fn should_have_world_accessors() {
-        let (_, mut context) = test_fixtures::init();
+        let (_, mut context) = init();
         { 
             let _world = context.world(); 
         }
@@ -103,63 +79,15 @@ mod context_world_tests {
             let _world = context.world_mut();
         }
     }
-}
-
-#[cfg(test)]
-mod context_resource_tests {
-    use super::*;
-    use test_fixtures::TestResource;
 
     #[test]
     fn should_have_resources_accessors() {
-        let (_, mut context) = test_fixtures::init();
+        let (_, mut context) = init();
         { 
             let _resources = context.resources(); 
         }
         {
             let _mut_resources = context.resources_mut();
         }
-    }
-
-    #[test]
-    fn should_add_resource() {
-        let (_, mut context) = test_fixtures::init();
-
-        context.insert_resource(TestResource("Hello, World!"));
-        let resource = context
-            .resource::<TestResource>()
-            .expect("Resource doesn't exist");
-
-        assert_eq!(resource.0, "Hello, World!");
-    }
-
-    #[test]
-    fn should_mutate_resource() {
-        let (_, mut context) = test_fixtures::init();
-        context.insert_resource(TestResource("Hello, World!"));
-
-        {
-            let mut resource = context
-                .resource_mut::<TestResource>()
-                .expect("Resource doesn't exist");
-            resource.0 = "Hello, changed World!";
-        }
-
-        let resource = context
-            .resource::<TestResource>()
-            .expect("Resource doesn't exist");
-        assert_eq!(resource.0, "Hello, changed World!");
-    }
-
-    #[test]
-    fn should_remove_resource() {
-        let (_, mut context) = test_fixtures::init();
-
-        context.insert_resource(TestResource("Hello, World!"));
-        assert!(context.resource::<TestResource>().is_some());
-
-        let resource = context.remove_resource::<TestResource>();
-        assert!(resource.is_some());
-        assert!(context.resource::<TestResource>().is_none());
     }
 }
