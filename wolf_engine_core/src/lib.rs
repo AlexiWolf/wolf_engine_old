dcf746eedaecedc8170ad9e67acc379110ca111e
AlexiWolf <alexi@wolvesin.space>
2023-09-30 12:29:55 -0500

    Documented `ResourcesBuilder` methods

--- wolf_engine_core/src/lib.rs
+++ wolf_engine_core/src/lib.rs
@@ -75,11 +75,18 @@ pub mod ecs {
     }
 
     impl ResourcesBuilder {
+        /// Inserts the provide instance of `T` into the [`Resources`].
+        /// 
+        /// If the provided type has previously been added, the existing instance is silently
+        /// overwritten.
+        ///
+        /// This function is functionally-identical to calling [`Resources::insert()`].
         pub fn add_resource<T: systems::Resource + 'static>(&mut self, resource: T) -> &mut Self {
             self.resources.insert(resource);
             self
         }
-
+        
+        /// Consumes the builder, and returns the [`Resources`] from it.
         pub fn build(self) -> Resources {
             self.resources
         }
