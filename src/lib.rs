1a8fc7b2130ed9f48f1012e3bd48d13425a31a67
AlexiWolf <alexi@wolvesin.space>
2023-10-01 13:01:36 -0500

    Moved `Crate Features` section to the bottom

--- src/lib.rs
+++ src/lib.rs
@@ -17,20 +17,20 @@
 //! wolf_engine = { git = "https://github.com/AlexiWolf/wolf_engine" }
 //! ```
 //!
-//! ## Crate Features
-//!
-//! - `framework`: Enable the high-level framework (enabled by default.)
-//! - `logging`: Enable built-in logging implementation.
-//! - `serde`: Enable [Serde](https://crates.io.crates/serde) support for some types.
-//! - `window`: Enable Wolf Engine's high-level window API.
-//!
-//! ## Usage Examples
+//! ## Usage 
 //! 
 //! - The [`framework` module](wolf_engine_framework), and [`core` module](wolf_engine_core) docs
 //!   have some helpful quick-start examples you can follow to quickly jump-start your project.
 //! - There are plenty of functional examples in the
 //!   [examples/ directory](https://github.com/AlexiWolf/wolf_engine/tree/main/examples) in the
 //!   repo.
+//!
+//! ## Crate Features
+//!
+//! - `framework`: Enable the high-level framework (enabled by default.)
+//! - `logging`: Enable built-in logging implementation.
+//! - `serde`: Enable [Serde](https://crates.io.crates/serde) support for some types.
+//! - `window`: Enable Wolf Engine's high-level window API.
 
 pub use wolf_engine_core::prelude::*;
 
