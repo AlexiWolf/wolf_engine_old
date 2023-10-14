a15c808246d871801a20969f06c3398b30554c8e
AlexiWolf <alexi@wolvesin.space>
2023-10-14 16:56:44 -0500

    Revised doc

--- wolf_engine_core/src/events/engine_events.rs
+++ wolf_engine_core/src/events/engine_events.rs
@@ -7,8 +7,7 @@ use super::EventSender;
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum WindowEvent {}
 
-/// An alias to the main [`EventSender`] associated with the current [`EventLoop`].  To make it
-/// easier to access.
+/// An alias to the main [`EventSender`] type associated with the [`EventLoop`](crate::EventLoop). 
 pub type MainEventSender<E> = Arc<dyn EventSender<Event<E>>>;
 
 /// A user-defined [`Event`] type.
