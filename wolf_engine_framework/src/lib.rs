bc6bf39de5163b8c45a2235f1721949731d7e99c
AlexiWolf <alexi@wolvesin.space>
2023-11-26 12:38:08 -0600

    Reordered imports

--- wolf_engine_framework/src/lib.rs
+++ wolf_engine_framework/src/lib.rs
@@ -8,9 +8,10 @@
 pub mod plugins;
 
 use plugins::Plugin;
+
+use wolf_engine_core::Engine;
 use wolf_engine_core::ecs::ResourcesBuilder;
 use wolf_engine_core::ecs::systems::Resource;
-use wolf_engine_core::Engine;
 use wolf_engine_core::events::UserEvent;
 
 pub struct FrameworkBuilder<E: UserEvent> {
