7ac3a41dc586b06a1a451b25632745e3218f1e27
AlexiWolf <alexi@wolvesin.space>
2023-11-26 20:03:16 -0600

    Removed the `ResourcesBuilder`

--- wolf_engine_core/src/ecs.rs
+++ wolf_engine_core/src/ecs.rs
@@ -6,29 +6,6 @@ pub use wolf_engine_codegen::system;
 #[doc(hidden)]
 pub mod prelude {
     pub use super::system;
-    pub use super::ResourcesBuilder;
     pub use super::Schedule;
 }
 
-/// Provides a builder-pattern for creating [`Resources`].
-#[derive(Default)]
-pub struct ResourcesBuilder {
-    resources: Resources,
-}
-
-impl ResourcesBuilder {
-    /// Inserts the provide instance of `T` into the [`Resources`].
-    ///
-    /// If the provided type has previously been added, the existing instance is silently
-    /// overwritten.
-    /// This function is functionally-identical to calling [`Resources::insert()`]. pub fn add_resource<T: systems::Resource + 'static>(&mut self, resource: T) -> &mut Self {
-    pub fn add_resource<T: systems::Resource + 'static>(&mut self, resource: T) -> &mut Self {
-        self.resources.insert(resource);
-        self
-    }
-
-    /// Consumes the builder, and returns the [`Resources`] from it.
-    pub fn build(self) -> Resources {
-        self.resources
-    }
-}
