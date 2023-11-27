cff7159d1b91ab7e1354f9ad84216a2ec278e067
AlexiWolf <alexi@wolvesin.space>
2023-11-27 11:15:16 -0600

    Added docs to the plugin types

--- wolf_engine_framework/src/plugins.rs
+++ wolf_engine_framework/src/plugins.rs
@@ -4,10 +4,25 @@ use crate::FrameworkBuilder;
 
 use wolf_engine_core::events::UserEvent;
 
+/// A result type for the plugin system.
 pub type PluginResult = Result<(), String>;
 
+/// A module which adds new functionality to the engine.
 pub trait Plugin<E: UserEvent> {
+    /// Returns a people-friendly name for the plugin.
+    ///
+    /// This is mostly used to identify the plugin in logs.  There aren't any specific requirements
+    /// for what the name should be, and it may change, so you probably shouldn't use this to
+    /// uniquely identify plugins.
     fn name(&self) -> &str;
+
+    /// Loads the plugin using the provided [`FrameworkBuilder`].
+    ///
+    /// The plugin does all setup here.
+    ///
+    /// **Note:** Plugins shouldn't try to load other plugins using the builder.  At this point in
+    /// the setup process, it's not possible to add additional plugins.  Nothing will happen if you
+    /// try.
     fn load(&mut self, builder: &mut FrameworkBuilder<E>) -> PluginResult;
 }
 
