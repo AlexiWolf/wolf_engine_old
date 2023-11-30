f21ad8a95b456aca76f813ea0a018fe4a76b77e2
AlexiWolf <alexi@wolvesin.space>
2023-11-29 18:06:31 -0600

    Added stub of `Scene` and `Stage`

--- wolf_engine_framework/src/scene.rs
+++ wolf_engine_framework/src/scene.rs
@@ -1,7 +1,30 @@
+use wolf_engine_core::events::UserEvent;
 
+#[cfg_attr(test, mockall::automock)]
+pub trait Scene<E: UserEvent> {
+
+}
+
+pub struct Stage<E: UserEvent> {
+    stack: Vec<Box<dyn Scene<E>>>, 
+}
+
+impl<E: UserEvent> Stage<E> {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, scene: Box<dyn Scene<E>>) {
+        
+    }
+}
 
 #[cfg(test)]
 mod scene_tests {
+    use super::*; 
+
     #[test]
     fn should_add_scene_to_stage() {
         let (_event_loop, context) = wolf_engine_core::init()
