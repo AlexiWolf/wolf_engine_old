34594ed1a40d7eb1c943f16934e82f1875efec77
AlexiWolf <alexi@wolvesin.space>
2023-12-25 16:54:24 -0600

    Removed use of `delegate!`

--- wolf_engine_framework/src/scenes/scene.rs
+++ wolf_engine_framework/src/scenes/scene.rs
@@ -89,26 +89,31 @@ impl<E: UserEvent> Scene<E, Unloaded> {
 }
 
 impl<E: UserEvent> Scene<E, Loaded> {
-    delegate! {
-        to self.inner {
-            /// Updates the game state when the scene is active.
-            ///
-            /// Active updates can optionally return a [`SceneChange`](crate::scenes::SceneChange), to the
-            /// [`Stage`](crate::scenes::Stage) to change scenes.
-            pub fn update(&mut self, context: &mut Context<E>) -> Option<SceneChange<E>>;
+    /// Updates the game state when the scene is active.
+    ///
+    /// Active updates can optionally return a [`SceneChange`](crate::scenes::SceneChange), to the
+    /// [`Stage`](crate::scenes::Stage) to change scenes.
+    pub fn update(&mut self, context: &mut Context<E>) -> Option<SceneChange<E>> {
+        self.inner.update(context)
+    }
+    /// Renders the current game state when the scene is active.
+    pub fn render(&mut self, context: &mut Context<E>) {
+        self.inner.render(context)
+    }
 
-            /// Renders the current game state when the scene is active.
-            pub fn render(&mut self, context: &mut Context<E>);
+    /// Updates the current state when the scene is in the background.
+    pub fn background_update(&mut self, context: &mut Context<E>) {
+        self.inner.background_update(context)
+    }
 
-            /// Updates the current state when the scene is in the background.
-            pub fn background_update(&mut self, context: &mut Context<E>);
+    /// Renders the current state when the scene is in the background.
+    pub fn background_render(&mut self, context: &mut Context<E>) {
+        self.inner.background_render(context)
+    }
 
-            /// Renders the current state when the scene is in the background.
-            pub fn background_render(&mut self, context: &mut Context<E>);
-
-            /// Unloads the scene, consuming, and dropping it in the process.
-            pub fn unload(mut self, context: &mut Context<E>);
-        }
+    /// Unloads the scene, consuming, and dropping it in the process.
+    pub fn unload(mut self, context: &mut Context<E>) {
+        self.inner.unload(context)
     }
 }
 
