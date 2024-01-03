71dff452f3a21452f49ad800010e425b254917c8
AlexiWolf <alexi@wolvesin.space>
2024-01-02 21:25:16 -0600

    Added auto-impl `EventTrait`

--- engine/wolf_engine_core/src/events/mod.rs
+++ engine/wolf_engine_core/src/events/mod.rs
@@ -3,7 +3,29 @@
 //! Wolf Engine re-exports [Generic Event Queue](generic_event_queue), see the original crate for
 //! details.
 
+use downcast_rs::*;
 pub use generic_event_queue::*;
 
 mod event;
 pub use event::*;
+
+pub trait EventTrait: Downcast + 'static {}
+impl_downcast!(EventTrait);
+
+impl<T> EventTrait for T where T: 'static {}
+
+#[cfg(test)]
+mod event_tests {
+    use test_case::test_case;
+
+    use super::*;
+
+    struct TestEvent(&'static str);
+    
+    #[test_case(&TestEvent("Hello, World!"))]
+    fn should_auto_impl_event(event: &dyn EventTrait) {
+        if let Some(event) = event.downcast_ref::<TestEvent>() {
+            assert_eq!(event.0, "Hello, World!");
+        }
+    }
+}
