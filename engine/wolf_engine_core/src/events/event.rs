af22c2073da3f5889433ed4259744940cc2a733a
AlexiWolf <alexi@wolvesin.space>
2024-01-08 07:26:38 -0600

    Removed `Copy` / `Clone` impls from `EngineEvent`

--- engine/wolf_engine_core/src/events/event.rs
+++ engine/wolf_engine_core/src/events/event.rs
@@ -2,7 +2,7 @@ use crate::events::Event;
 
 /// Provides the main events used by Wolf Engine.
 #[non_exhaustive]
-#[derive(Event, Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Event, Debug, PartialEq, Eq)]
 pub enum EngineEvent {
     /// Emitted when the engine should quit.
     Quit,
@@ -11,27 +11,4 @@ pub enum EngineEvent {
     ///
     /// `EventsCleared` should be emitted only after all other events have been processed.
     EventsCleared,
-}
-
-#[cfg(test)]
-mod event_tests {
-    use super::*;
-
-    #[test]
-    fn should_implement_clone() {
-        let event: EngineEvent = EngineEvent::EventsCleared;
-        let clone = event.clone();
-        assert_eq!(event, clone);
-    }
-
-    #[test]
-    fn should_implement_copy() {
-        let event: EngineEvent = EngineEvent::EventsCleared;
-        let copy = copy_test(event);
-        assert_eq!(event, copy);
-    }
-
-    fn copy_test(event: EngineEvent) -> EngineEvent {
-        event
-    }
 }
