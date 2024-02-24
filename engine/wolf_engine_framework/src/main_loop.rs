0324f9cab4a2c8f05e932cca27a04ed3ed4868a0
AlexiWolf <alexi@wolvesin.space>
2024-02-24 10:35:51 -0600

    Re-added existing `MainLoop` impl for `FnMut(Engine)`

--- engine/wolf_engine_framework/src/main_loop.rs
+++ engine/wolf_engine_framework/src/main_loop.rs
@@ -6,3 +6,12 @@ pub trait MainLoop {
     /// Runs the main-loop until the engine quits.
     fn run(&mut self, engine: Engine);
 }
+
+impl<T> MainLoop for T
+where
+    T: FnMut(Engine),
+{
+    fn run(&mut self, engine: Engine) {
+        (self)(engine)
+    }
+}
