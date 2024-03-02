54a03ac356271778875f2f60f6067a92435bcab0
AlexiWolf <alexi@wolvesin.space>
2024-03-02 09:15:26 -0600

    Added `winit` window setup

--- examples/graphics.rs
+++ examples/graphics.rs
@@ -1,8 +1,14 @@
+use winit::{window::WindowBuilder, event_loop::EventLoop, dpi::PhysicalSize};
 use wolf_engine::prelude::*;
 
 pub fn main() {
     env_logger::init();
+    let winit_event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Wolf Engine - Graphics Example")
+        .with_inner_size(PhysicalSize::new(800, 600))
+        .build(&winit_event_loop)
+        .unwrap();
     let graphics_settings = GraphicsSettings::default();
-    let window = None;
-    let graphics = pollster::block_on(wolf_engine::graphics::init(graphics_settings, window));
+    let graphics = pollster::block_on(wolf_engine::graphics::init(graphics_settings, Some(&window)));
 }
