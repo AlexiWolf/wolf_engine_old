d55ae2d3362f8151a515bfbc604da8d98b47332e
AlexiWolf <alexi@wolvesin.space>
2024-03-02 19:07:52 -0600

    Moved window creation to `Resumed` event

--- examples/graphics.rs
+++ examples/graphics.rs
@@ -1,22 +1,26 @@
-use winit::{dpi::PhysicalSize, event_loop::EventLoop, window::WindowBuilder};
+use winit::{dpi::PhysicalSize, event_loop::EventLoop, window::WindowBuilder, event::{Event, WindowEvent}};
 use wolf_engine::prelude::*;
 
 pub fn main() {
     env_logger::init();
     let winit_event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new()
-        .with_title("Wolf Engine - Graphics Example")
-        .with_inner_size(PhysicalSize::new(800, 600))
-        .build(&winit_event_loop)
-        .unwrap();
+    let mut window = None;
     let graphics = pollster::block_on(wolf_engine::graphics::init(
         GraphicsSettings::default(),
-        Some(&window),
+        None,
     ));
 
     let _ = winit_event_loop.run(|event, window_target| {
         match event {
             Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => window_target.exit(),
+            Event::Resumed => {
+                window = Some(WindowBuilder::new()
+                    .with_title("Wolf Engine - Graphics Example")
+                    .with_inner_size(PhysicalSize::new(800, 600))
+                    .build(&window_target)
+                    .unwrap()
+                );
+            },
             _ => (),
         }
     });
